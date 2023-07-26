a1bbe8805843041433bf710140010ef63d06e543
Chyharov <102535915+Chyharov@users.noreply.github.com>
2023-07-26 22:14:48 +0000

    Deploying to gh-pages from @ Chyharov/alpha-and-omega@6309cd7bd07aac50bef65b4cd55243d25ad6ad92 🚀

---
+++ static/js/519.181265b8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkalpha_and_omega=self.webpackChunkalpha_and_omega||[]).push([[519],{3519:function(a,e,o){o.d(e,{Z:function(){return r}});var l=o(1087);o(2791);var n=o.p+"static/media/omega-ritual-logo.0259abe9a7624f72a96aaee1faafa651.svg",s="Logo_logo__6yrBi",_="Logo_logo__img__zmYBe",g=o(184),r=function(){return(0,g.jsx)(l.rU,{className:s,to:"/",children:(0,g.jsx)("img",{className:_,loading:"lazy",src:n,alt:"header__logo"})})}}}]);
+//# sourceMappingURL=519.181265b8.chunk.js.map
