7a5de3739793c3702b08e867ba44191d7c84754a
myusername <chyharov@gmail.com>
2023-08-05 21:58:15 +0300

    UPDATE: logo for header and section about

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,14 +1,11 @@
 import { Link } from 'react-router-dom';
-// import logo from '../../images/logo/omega-ritual-logo.svg';
-import logo from '../../images/logo/alpha-logo.svg';
-import logo2 from '../../images/logo/alpha-ritual.svg';
+import logo from '../../images/logo/alpha-ritual-logo.svg';
 import s from './Logo.module.scss';
 
 const Logo = () => {
   return (
     <Link className={s.logo} to="/">
       <img className={s.logo__img} loading="lazy" src={logo} alt="header__logo" />
-      <img className={s.logo__img2} loading="lazy" src={logo2} alt="header__logo" />
   </Link>
     
         
