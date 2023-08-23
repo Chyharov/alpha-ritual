3e7de3489a8e37dbe40711c5123ee29cec12e2db
myusername <chyharov@gmail.com>
2023-08-23 19:09:37 +0300

    CHANGE: logo for header and section about

--- src/components/SectionAbout/SectionAbout.jsx
+++ src/components/SectionAbout/SectionAbout.jsx
@@ -1,4 +1,4 @@
-import logo from '../../images/logo/logoAbout.svg';
+import logo from '../../images/logo/alpha-ritual-logo.svg';
 import s from '../SectionAbout/SectionAbout.module.scss';
 
 const SectionAbout = () => {
@@ -9,7 +9,7 @@ const SectionAbout = () => {
           <svg className={s.about__logo}>
             <image className={s.image__logo} xlinkHref={logo} alt="Logo"/>
           </svg>
-          <h1 className="title">АЛЬФА-РИТУАЛ</h1>
+          <h1 className={s.title__about}>АЛЬФА-РИТУАЛ</h1>
           <p className="description" style={{ textAlign: 'center', marginBottom: '20px' }}>похоронний дім</p>
           <p className={s.about__company}>Похоронний дім Альфа має у своєму розпорядженні всі необхідні товари та послуги, що дозволяють проводити поховання або кремацію будь-якого рівня.</p>
         </div>
