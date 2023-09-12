fbe1a5f937bd0393560b7a9895402907126838c0
myusername <chyharov@gmail.com>
2023-09-12 20:52:40 +0300

    ADDED: new component for section coffins - elite price list

---
+++ src/components/CoffinsPictureElitePriceList/CoffinsPictureElitePriceList.jsx
@@ -0,0 +1,69 @@
+import s from './CoffinsPictureElitePriceList.module.scss'
+
+const CoffinsPictureElitePriceList = ({ openModal }) => {
+    
+    const CoffinsPictureElitePriceList = [
+        { id: 1, src: require('../../images/coffinsElitePrice/coffinsElitePrice3257.webp'), alt: 'Зображення 1' },
+        { id: 2, src: require('../../images/coffinsElitePrice/coffinsElitePrice3258.webp'), alt: 'Зображення 2' },
+        { id: 3, src: require('../../images/coffinsElitePrice/coffinsElitePrice3264.webp'), alt: 'Зображення 3' },
+        { id: 4, src: require('../../images/coffinsElitePrice/coffinsElitePrice3273.webp'), alt: 'Зображення 4' },
+        { id: 5, src: require('../../images/coffinsElitePrice/coffinsElitePrice3282.webp'), alt: 'Зображення 5' },
+        { id: 6, src: require('../../images/coffinsElitePrice/coffinsElitePrice3283.webp'), alt: 'Зображення 6' },
+        { id: 7, src: require('../../images/coffinsElitePrice/coffinsElitePrice3292.webp'), alt: 'Зображення 7' },
+        { id: 8, src: require('../../images/coffinsElitePrice/coffinsElitePrice3295.webp'), alt: 'Зображення 8' },
+        { id: 9, src: require('../../images/coffinsElitePrice/coffinsElitePrice3301.webp'), alt: 'Зображення 9' },
+        { id: 10, src: require('../../images/coffinsElitePrice/coffinsElitePrice3304.webp'), alt: 'Зображення 10' },
+        { id: 11, src: require('../../images/coffinsElitePrice/coffinsElitePrice3310.webp'), alt: 'Зображення 11' },
+        { id: 12, src: require('../../images/coffinsElitePrice/coffinsElitePrice3312.webp'), alt: 'Зображення 12' },
+        { id: 13, src: require('../../images/coffinsElitePrice/coffinsElitePrice3323.webp'), alt: 'Зображення 13' },
+        { id: 14, src: require('../../images/coffinsElitePrice/coffinsElitePrice3328.webp'), alt: 'Зображення 14' },
+        { id: 15, src: require('../../images/coffinsElitePrice/coffinsElitePrice3332.webp'), alt: 'Зображення 15' },
+        { id: 16, src: require('../../images/coffinsElitePrice/coffinsElitePrice3334.webp'), alt: 'Зображення 16' },
+        { id: 17, src: require('../../images/coffinsElitePrice/coffinsElitePrice3336.webp'), alt: 'Зображення 17' },
+        { id: 18, src: require('../../images/coffinsElitePrice/coffinsElitePrice3338.webp'), alt: 'Зображення 18' },
+        { id: 19, src: require('../../images/coffinsElitePrice/coffinsElitePrice3340.webp'), alt: 'Зображення 19' },
+        { id: 20, src: require('../../images/coffinsElitePrice/coffinsElitePrice3341.webp'), alt: 'Зображення 20' },
+        { id: 22, src: require('../../images/coffinsElitePrice/coffinsElitePrice3342.webp'), alt: 'Зображення 22' },
+        { id: 23, src: require('../../images/coffinsElitePrice/coffinsElitePrice3345.webp'), alt: 'Зображення 23' },
+        { id: 24, src: require('../../images/coffinsElitePrice/coffinsElitePrice3349.webp'), alt: 'Зображення 24' },
+        { id: 25, src: require('../../images/coffinsElitePrice/coffinsElitePrice3352.webp'), alt: 'Зображення 25' },
+        { id: 26, src: require('../../images/coffinsElitePrice/coffinsElitePrice3353.webp'), alt: 'Зображення 26' },
+        { id: 27, src: require('../../images/coffinsElitePrice/coffinsElitePrice3406.webp'), alt: 'Зображення 27' },
+        { id: 28, src: require('../../images/coffinsElitePrice/coffinsElitePrice3409.webp'), alt: 'Зображення 28' },
+        { id: 29, src: require('../../images/coffinsElitePrice/coffinsElitePrice3422.webp'), alt: 'Зображення 29' },
+    ];
+
+    return (
+        <section className={s.CoffinsPictureElitePriceListWreaths}>
+                <h2 className="title" style={{ marginBottom: '20px' }}>Елітні дерев’яні труни та саркофаги</h2>                  
+                <p className="description" style={{marginBottom: '20px' }}>Спосіб життя та навколишні людини предмети демонструють його статус і становище у суспільстві. Елітні автомобілі та нерухомість, дорогі предмети гардеробу та аксесуари – все це характерні атрибути високої статусності. Похорон – це проводи померлої людини в останній шлях, а значить, це остання можливість віддати їй всю повноту почестей відповідно до її прижиттєвого статусу.</p>
+
+                <p className="description" style={{marginBottom: '20px' }}>Елітна труна з цінних порід дерева ручної роботи з ідеальним опрацюванням найдрібніших деталей декору та фурнітури, бездоганним поліруванням та лакуванням – це демонстрація не просто статусності покійного, а й безмежної поваги до його близьких рідних, друзів та колег.</p>
+                
+                <p className="description" style={{marginBottom: '20px' }}><b>Похоронний дім «Альфа»</b> – це понад 25 видів елітних дерев’яних трун власного виробництва. Завдяки відсутності посередників та власної майстерні ми пропонуємо ціни від виробника без зайвих націнок. Це може бути як стандартна дерев’яна лакована труна з елітних порід дерева з цільною кришкою, так і двокришковий саркофаг. Верхня кришка дозволяє відкрити на похороні обличчя та руки покійного, залишивши нижню частину прихованої тіла.</p>
+                
+                <p className="description" style={{marginBottom: '20px' }}>Ціна елітної лакованої дерев’яної труни або двокришкового саркофага залежить від породи дерева, глибини ручного опрацювання декору, якості лакофарбових матеріалів, оббивних тканин та фурнітури.</p>
+                
+                <ul className={s.coffins__list}>
+                    <li className={s.coffins__listItem}><p className="description" style={{marginBottom: '20px' }}>Для елітної внутрішньої оббивки та зовнішнього текстильного декору ми використовуємо Італійські тканини найвищої якості.</p></li>
+                    <li className={s.coffins__listItem}><p className="description" style={{marginBottom: '20px' }}>Тільки високоміцна престижна фурнітура, включаючи ручки та замки з дорогих металів.</p></li>
+                    <li className={s.coffins__listItem}><p className="description" style={{marginBottom: '20px' }}>Покриття дерева тільки найкращим лаком у кілька шарів, що робить поверхню ідеально рівною та з багатим блиском.</p></li>
+                </ul>
+                
+                <p className="description" style={{marginBottom: '20px' }}>Кожна така труна робиться майстрами кілька днів, через що її вартість виходить порівняно високою. При цьому ми не женемося за надприбутком і пропонуємо чесні ціни, які повністю відображають всю повноту копіткої багатоденної праці майстрів і елітних матеріалів, що використовуються.</p>
+
+                <h2 className="title" style={{ marginBottom: '20px' }}>Елітні труни</h2>
+                <p className="description" style={{marginBottom: '20px' }}><b>Матеріал:</b> дерев’яна заготовка покрита лаком</p>
+                    
+                <ul className={s.coffins__pictureList}>
+                    {CoffinsPictureElitePriceList.map((image) => (
+                    <li className={s.coffins__pictureListItem} key={image.id}>
+                        <img className={s.coffinsPictureElitePriceList__img} src={image.src} alt={image.alt} onClick={() => openModal(image.src, image.alt)}/>
+                    </li>
+                    ))}
+                </ul>
+        </section>
+    );
+  };
+
+export default CoffinsPictureElitePriceList;
--- src/components/SectionCoffins/SectionCoffins.jsx
+++ src/components/SectionCoffins/SectionCoffins.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import CoffinsPictureElitePriceList from 'components/CoffinsPictureElitePriceList/CoffinsPictureElitePriceList';
 import ModalWindow from 'components/ModalWindow/ModalWindow';
 import s from './SectionCoffins.module.scss'
 
@@ -7,37 +8,6 @@ const SectionCoffins = () => {
     const [modalOpen, setModalOpen] = useState(false);
     const [selectedImageSrc, setSelectedImageSrc] = useState('');
     const [selectedImageAlt, setSelectedImageAlt] = useState('');
-    
-    const CoffinsPictureElitePriceList = [
-        { id: 1, src: require('../../images/coffinsElitePrice/coffinsElitePrice3257.webp'), alt: 'Зображення 1' },
-        { id: 2, src: require('../../images/coffinsElitePrice/coffinsElitePrice3258.webp'), alt: 'Зображення 2' },
-        { id: 3, src: require('../../images/coffinsElitePrice/coffinsElitePrice3264.webp'), alt: 'Зображення 3' },
-        { id: 4, src: require('../../images/coffinsElitePrice/coffinsElitePrice3273.webp'), alt: 'Зображення 4' },
-        { id: 5, src: require('../../images/coffinsElitePrice/coffinsElitePrice3282.webp'), alt: 'Зображення 5' },
-        { id: 6, src: require('../../images/coffinsElitePrice/coffinsElitePrice3283.webp'), alt: 'Зображення 6' },
-        { id: 7, src: require('../../images/coffinsElitePrice/coffinsElitePrice3292.webp'), alt: 'Зображення 7' },
-        { id: 8, src: require('../../images/coffinsElitePrice/coffinsElitePrice3295.webp'), alt: 'Зображення 8' },
-        { id: 9, src: require('../../images/coffinsElitePrice/coffinsElitePrice3301.webp'), alt: 'Зображення 9' },
-        { id: 10, src: require('../../images/coffinsElitePrice/coffinsElitePrice3304.webp'), alt: 'Зображення 10' },
-        { id: 11, src: require('../../images/coffinsElitePrice/coffinsElitePrice3310.webp'), alt: 'Зображення 11' },
-        { id: 12, src: require('../../images/coffinsElitePrice/coffinsElitePrice3312.webp'), alt: 'Зображення 12' },
-        { id: 13, src: require('../../images/coffinsElitePrice/coffinsElitePrice3323.webp'), alt: 'Зображення 13' },
-        { id: 14, src: require('../../images/coffinsElitePrice/coffinsElitePrice3328.webp'), alt: 'Зображення 14' },
-        { id: 15, src: require('../../images/coffinsElitePrice/coffinsElitePrice3332.webp'), alt: 'Зображення 15' },
-        { id: 16, src: require('../../images/coffinsElitePrice/coffinsElitePrice3334.webp'), alt: 'Зображення 16' },
-        { id: 17, src: require('../../images/coffinsElitePrice/coffinsElitePrice3336.webp'), alt: 'Зображення 17' },
-        { id: 18, src: require('../../images/coffinsElitePrice/coffinsElitePrice3338.webp'), alt: 'Зображення 18' },
-        { id: 19, src: require('../../images/coffinsElitePrice/coffinsElitePrice3340.webp'), alt: 'Зображення 19' },
-        { id: 20, src: require('../../images/coffinsElitePrice/coffinsElitePrice3341.webp'), alt: 'Зображення 20' },
-        { id: 22, src: require('../../images/coffinsElitePrice/coffinsElitePrice3342.webp'), alt: 'Зображення 22' },
-        { id: 23, src: require('../../images/coffinsElitePrice/coffinsElitePrice3345.webp'), alt: 'Зображення 23' },
-        { id: 24, src: require('../../images/coffinsElitePrice/coffinsElitePrice3349.webp'), alt: 'Зображення 24' },
-        { id: 25, src: require('../../images/coffinsElitePrice/coffinsElitePrice3352.webp'), alt: 'Зображення 25' },
-        { id: 26, src: require('../../images/coffinsElitePrice/coffinsElitePrice3353.webp'), alt: 'Зображення 26' },
-        { id: 27, src: require('../../images/coffinsElitePrice/coffinsElitePrice3406.webp'), alt: 'Зображення 27' },
-        { id: 28, src: require('../../images/coffinsElitePrice/coffinsElitePrice3409.webp'), alt: 'Зображення 28' },
-        { id: 29, src: require('../../images/coffinsElitePrice/coffinsElitePrice3422.webp'), alt: 'Зображення 29' },
-    ];
     
     const CoffinsPictureAveragePriceList = [
         { id: 1, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3365.webp'), alt: 'Зображення 1' },
@@ -109,40 +79,14 @@ const SectionCoffins = () => {
   });
 
     return (
-
         <section className={s.sectionCoffinsWreaths}>
           <div className={'container ' + s.coffinsContainer}>
             <div className={s.coffinsDescriptionContainer}>
               <p className="description" style={{marginBottom: '20px' }}><b>Похоронний дім «Альфа»</b> – це власне виробництво ритуальних трун, які виготовляються із якісної міцної деревини з використанням декоративних оббивних тканин вітчизняного та європейського виробництва. Ми не спекулюємо на горі, тому ціни на всі наші труни, включаючи бюджетні варіанти та елітні двокришкові саркофаги, одні з найдоступніших у Києві.</p>
                 <h2 className="title" style={{ marginBottom: '20px' }}>Від економ-класу до елітних саркофагів із дорогих порід дерева.</h2>
                 <p className="description" style={{marginBottom: '20px' }}>Труна – це обов’язковий ритуальний атрибут поховання в більшості культур та релігій світу. На відміну від ісламу, де прийнято надавати тіло землі в тканинах та багатих килимах, або індуїзму, де покійного кремують у поховальному савані, у християнстві померлих ховають у дерев’яних трунах із багатим декоративним оздобленням. Урочисті шати покійного, атласні, оксамитові та шовкові тканини внутрішньої та зовнішньої оббивки – все це символ останніх почестей, наданих покійному в його останньому шляху.</p>    
-                <h2 className="title" style={{ marginBottom: '20px' }}>Елітні дерев’яні труни та саркофаги</h2>                  
-                <p className="description" style={{marginBottom: '20px' }}>Спосіб життя та навколишні людини предмети демонструють його статус і становище у суспільстві. Елітні автомобілі та нерухомість, дорогі предмети гардеробу та аксесуари – все це характерні атрибути високої статусності. Похорон – це проводи померлої людини в останній шлях, а значить, це остання можливість віддати їй всю повноту почестей відповідно до її прижиттєвого статусу.</p>
-
-                <p className="description" style={{marginBottom: '20px' }}>Елітна труна з цінних порід дерева ручної роботи з ідеальним опрацюванням найдрібніших деталей декору та фурнітури, бездоганним поліруванням та лакуванням – це демонстрація не просто статусності покійного, а й безмежної поваги до його близьких рідних, друзів та колег.</p>
                 
-                <p className="description" style={{marginBottom: '20px' }}><b>Похоронний дім «Альфа»</b> – це понад 25 видів елітних дерев’яних трун власного виробництва. Завдяки відсутності посередників та власної майстерні ми пропонуємо ціни від виробника без зайвих націнок. Це може бути як стандартна дерев’яна лакована труна з елітних порід дерева з цільною кришкою, так і двокришковий саркофаг. Верхня кришка дозволяє відкрити на похороні обличчя та руки покійного, залишивши нижню частину прихованої тіла.</p>
-                
-                <p className="description" style={{marginBottom: '20px' }}>Ціна елітної лакованої дерев’яної труни або двокришкового саркофага залежить від породи дерева, глибини ручного опрацювання декору, якості лакофарбових матеріалів, оббивних тканин та фурнітури.</p>
-                
-                <ul className={s.coffins__list}>
-                    <li className={s.coffins__listItem}><p className="description" style={{marginBottom: '20px' }}>Для елітної внутрішньої оббивки та зовнішнього текстильного декору ми використовуємо Італійські тканини найвищої якості.</p></li>
-                    <li className={s.coffins__listItem}><p className="description" style={{marginBottom: '20px' }}>Тільки високоміцна престижна фурнітура, включаючи ручки та замки з дорогих металів.</p></li>
-                    <li className={s.coffins__listItem}><p className="description" style={{marginBottom: '20px' }}>Покриття дерева тільки найкращим лаком у кілька шарів, що робить поверхню ідеально рівною та з багатим блиском.</p></li>
-                </ul>
-                
-                <p className="description" style={{marginBottom: '20px' }}>Кожна така труна робиться майстрами кілька днів, через що її вартість виходить порівняно високою. При цьому ми не женемося за надприбутком і пропонуємо чесні ціни, які повністю відображають всю повноту копіткої багатоденної праці майстрів і елітних матеріалів, що використовуються.</p>
-
-                <h2 className="title" style={{ marginBottom: '20px' }}>Елітні труни</h2>
-                <p className="description" style={{marginBottom: '20px' }}><b>Матеріал:</b> дерев’яна заготовка покрита лаком</p>
-                    
-                <ul className={s.coffins__pictureList}>
-                    {CoffinsPictureElitePriceList.map((image) => (
-                    <li className={s.coffins__pictureListItem} key={image.id}>
-                        <img className={s.coffinsPictureElitePriceList__img} src={image.src} alt={image.alt} onClick={() => openModal(image.src, image.alt)}/>
-                    </li>
-                    ))}
-                </ul>
+            <CoffinsPictureElitePriceList openModal={openModal} />
                 
                 <h2 className="title" style={{ marginBottom: '20px' }}>Стандартні та недорогі бюджетні труни</h2>
                 <p className="description" style={{marginBottom: '20px' }}>Вартість стандартної дерев’яної ритуальної труни середнього цінового рівня в нашому похоронному бюро також є однією з найдоступніших у Києві завдяки чесному відношенню до виробництва та ціноутворення. У виробництві недорогих стандартних моделей середньоцінового рівня ми використовуємо недорогу, але міцну деревину, яка після ретельної обробки набуває урочистого вигляду, що відповідає траурному заходу. Внутрішня оббивка виконується за допомогою недорогого, але якісного та приємного на вигляд текстилю.</p>    
