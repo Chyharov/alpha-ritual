d7181fc95fdfba13cd6b932709cc0fd691b349e7
myusername <chyharov@gmail.com>
2023-10-13 21:07:51 +0300

    ADDED: component CarouselModal and separate SectionDecorations

---
+++ src/components/CarouselModal/CarouselModal.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect } from 'react';
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { AiOutlineClose } from 'react-icons/ai';
+import s from './CarouselModal.module.scss';
+
+const CarouselModal = ({ closeModal, currentSlide, decorationsPhotos }) => {
+  const handleKeyDown = (event) => {
+    if (event.key === "Escape") {
+      closeModal();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  });
+
+  const handleClick = (event) => {
+    if (event.target === event.currentTarget) {
+      closeModal();
+    }
+  };
+
+  return (
+    <div className={s.modal} onClick={handleClick}>
+      <div className={s.modalContent}>
+        <span className={s.close} onClick={closeModal}>
+          <AiOutlineClose className={s.closeModalIcon} />
+        </span>
+        <CarouselProvider
+          naturalSlideWidth={280}
+          naturalSlideHeight={210}
+          totalSlides={decorationsPhotos.length}
+          infinite={true}
+          currentSlide={currentSlide}
+        >
+          <Slider>
+            {decorationsPhotos.map((photo, index) => (
+              <Slide key={index} index={index + 1}>
+                <img className={s.modalImage} src={photo.src} alt={`Slide ${index}`} />
+              </Slide>
+            ))}
+          </Slider>
+          <div className={s.carouselCenterButtonModal}>
+            <ButtonBack className={s.carouselButtonModal}>
+              <FontAwesomeIcon icon={faChevronLeft} />
+            </ButtonBack>
+            <ButtonNext className={s.carouselButtonModal}>
+              <FontAwesomeIcon icon={faChevronRight} />
+            </ButtonNext>
+          </div>
+        </CarouselProvider>
+      </div>
+    </div>
+  );
+};
+
+export default CarouselModal;
--- src/components/SectionDecorations/SectionDecorations.jsx
+++ src/components/SectionDecorations/SectionDecorations.jsx
@@ -1,10 +1,6 @@
-import React, { useState, useEffect } from "react";
-import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
-import 'pure-react-carousel/dist/react-carousel.es.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
-import { AiOutlineClose } from 'react-icons/ai';
-import s from './SectionDecorations.module.scss';
+import React, { useState } from "react";
+import s from "./SectionDecorations.module.scss"
+import Modal from "../CarouselModal/CarouselModal"
 
 const decorationsPhotos = [
   { id: 1, src: require('../../images/decorations/decoration1.webp'), alt: 'Зображення 1' },
@@ -15,61 +11,6 @@ const decorationsPhotos = [
   { id: 6, src: require('../../images/decorations/decoration6.webp'), alt: 'Зображення 6' },
   { id: 7, src: require('../../images/decorations/decoration7.webp'), alt: 'Зображення 7' },
 ];
-
-const Modal = ({ closeModal, currentSlide, image }) => {
-  const handleKeyDown = (event) => {
-    if (event.key === "Escape") {
-      closeModal();
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener("keydown", handleKeyDown);
-
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-    };
-  });
-
-  const handleClick = (event) => {
-    if (event.target === event.currentTarget) {
-      closeModal();
-    }
-  };
-
-  return (
-    <div className={s.modal} onClick={handleClick}>
-      <div className={s.modalContent}>
-        <span className={s.close} onClick={closeModal}>
-          <AiOutlineClose className={s.closeModalIcon} />
-        </span>
-        <CarouselProvider
-          naturalSlideWidth={280}
-          naturalSlideHeight={210}
-          totalSlides={decorationsPhotos.length}
-          infinite={true}
-          currentSlide={currentSlide}
-        >
-          <Slider>
-            {decorationsPhotos.map((photo, index) => (
-              <Slide key={index} index={index + 1}>
-                <img className={s.modalImage} src={photo.src} alt={`Slide ${index}`} />
-              </Slide>
-            ))}
-          </Slider>
-          <div className={s.carouselCenterButtonModal}>
-            <ButtonBack className={s.carouselButtonModal}>
-              <FontAwesomeIcon icon={faChevronLeft} />
-            </ButtonBack>
-            <ButtonNext className={s.carouselButtonModal}>
-              <FontAwesomeIcon icon={faChevronRight} />
-            </ButtonNext>
-          </div>
-        </CarouselProvider>
-      </div>
-    </div>
-  );
-};
 
 const SectionDecorations = () => {
   const [modalImage, setModalImage] = useState(null);
@@ -100,7 +41,7 @@ const SectionDecorations = () => {
             ))}
           </ul>
           {isOpen && modalImage && (
-            <Modal image={modalImage} closeModal={closeModal} currentSlide={decorationsPhotos.indexOf(modalImage)} />
+            <Modal closeModal={closeModal} currentSlide={decorationsPhotos.indexOf(modalImage)} decorationsPhotos={decorationsPhotos} />
           )}
         </div>
       </div>
