d4b6659b67278e1df33d1495291647ec597251e2
myusername <chyharov@gmail.com>
2023-10-16 00:09:04 +0300

    ADDED: new component CoffinsPictureAveragePrice for SectionCoffins

---
+++ src/components/CoffinsPictureAveragePriceList/CoffinsPictureAveragePriceList.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import Modal from "../CarouselModal/CarouselModal"
+import s from './CoffinsPictureAveragePriceList.module.scss'
+
+    const CoffinsPictureAveragePrice = [
+        { id: 1, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3365.webp'), alt: 'Зображення 1', width: '180', height: '120' },
+        { id: 2, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3370.webp'), alt: 'Зображення 2', width: '180', height: '120' },
+        { id: 3, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3374.webp'), alt: 'Зображення 3', width: '180', height: '120' },
+        { id: 4, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3375.webp'), alt: 'Зображення 4', width: '180', height: '120' },
+        { id: 5, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3378.webp'), alt: 'Зображення 5', width: '180', height: '120' },
+        { id: 6, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3379.webp'), alt: 'Зображення 6', width: '180', height: '120' },
+        { id: 7, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3384.webp'), alt: 'Зображення 7', width: '180', height: '120' },
+        { id: 8, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3388.webp'), alt: 'Зображення 8', width: '180', height: '120' },
+        { id: 9, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3390.webp'), alt: 'Зображення 9', width: '180', height: '120' },
+        { id: 10, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3395.webp'), alt: 'Зображення 10', width: '180', height: '120' },
+        { id: 11, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3399.webp'), alt: 'Зображення 11', width: '180', height: '120' },
+        { id: 12, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3383.webp'), alt: 'Зображення 12', width: '180', height: '120' },    
+  ];
+
+const CoffinsPictureAveragePriceList = () => {
+
+  const [modalImage, setModalImage] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = (image) => {
+    setModalImage(image);
+    setIsOpen(true);
+    document.body.style.overflow = 'hidden';
+  };
+
+  const closeModal = () => {
+    setModalImage(null);
+    setIsOpen(false);
+    document.body.style.overflow = 'auto';
+  };
+
+  return (
+        <section className={s.CoffinsPictureAveragePriceWreaths}>
+            <h2 className="title" style={{ marginBottom: '20px' }}>Стандартні та недорогі бюджетні труни</h2>
+                <p className="description" style={{marginBottom: '20px' }}>Вартість стандартної дерев’яної ритуальної труни середнього цінового рівня в нашому похоронному бюро також є однією з найдоступніших у Києві завдяки чесному відношенню до виробництва та ціноутворення. У виробництві недорогих стандартних моделей середньоцінового рівня ми використовуємо недорогу, але міцну деревину, яка після ретельної обробки набуває урочистого вигляду, що відповідає траурному заходу. Внутрішня оббивка виконується за допомогою недорогого, але якісного та приємного на вигляд текстилю.</p>    
+                <p className="description" style={{marginBottom: '20px' }}><b>Матеріал:</b> дерев’яна заготовка, з елементами дерева, покритого лаком</p>
+                
+                <ul className={s.coffins__pictureList}>
+                    {CoffinsPictureAveragePrice.map((image) => (
+                    <li key={image.id}>
+                        <img className={s.coffinsPictureElitePriceList__img} src={image.src}
+                        alt={image.alt}
+                        onClick={() => openModal(image)}
+                      />
+                    </li>
+                    ))}
+                </ul>
+      
+            {isOpen && modalImage && (
+            <Modal
+              closeModal={closeModal}
+              currentSlide={CoffinsPictureAveragePrice.indexOf(modalImage)}
+              arrayPhotos={CoffinsPictureAveragePrice}
+            />
+            )}
+    
+        </section>
+    );
+  };
+
+export default CoffinsPictureAveragePriceList;
--- src/components/SectionCoffins/SectionCoffins.jsx
+++ src/components/SectionCoffins/SectionCoffins.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import CoffinsPictureElitePriceList from 'components/CoffinsPictureElitePriceList/CoffinsPictureElitePriceList';
+import CoffinsPictureAveragePriceList from 'components/CoffinsPictureAveragePriceList/CoffinsPictureAveragePriceList';
 import ModalWindow from 'components/ModalWindow/ModalWindow';
 import s from './SectionCoffins.module.scss'
 
@@ -8,21 +9,6 @@ const SectionCoffins = () => {
     const [modalOpen, setModalOpen] = useState(false);
     const [selectedImageSrc, setSelectedImageSrc] = useState('');
     const [selectedImageAlt, setSelectedImageAlt] = useState('');
-    
-    const CoffinsPictureAveragePriceList = [
-        { id: 1, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3365.webp'), alt: 'Зображення 1' },
-        { id: 2, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3370.webp'), alt: 'Зображення 2' },
-        { id: 3, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3374.webp'), alt: 'Зображення 3' },
-        { id: 4, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3375.webp'), alt: 'Зображення 4' },
-        { id: 5, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3378.webp'), alt: 'Зображення 5' },
-        { id: 6, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3379.webp'), alt: 'Зображення 6' },
-        { id: 7, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3384.webp'), alt: 'Зображення 7' },
-        { id: 8, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3388.webp'), alt: 'Зображення 8' },
-        { id: 9, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3390.webp'), alt: 'Зображення 9' },
-        { id: 10, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3395.webp'), alt: 'Зображення 10' },
-        { id: 11, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3399.webp'), alt: 'Зображення 11' },
-        { id: 12, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3383.webp'), alt: 'Зображення 12' },    
-  ];
     
     const CoffinsPictureEconomPriceList = [
         { id: 1, src: require('../../images/coffinsEconomPrice/coffinsEconomPrice3438.webp'), alt: 'Зображення 1' },
@@ -88,17 +74,7 @@ const SectionCoffins = () => {
                 
             <CoffinsPictureElitePriceList />
                 
-                <h2 className="title" style={{ marginBottom: '20px' }}>Стандартні та недорогі бюджетні труни</h2>
-                <p className="description" style={{marginBottom: '20px' }}>Вартість стандартної дерев’яної ритуальної труни середнього цінового рівня в нашому похоронному бюро також є однією з найдоступніших у Києві завдяки чесному відношенню до виробництва та ціноутворення. У виробництві недорогих стандартних моделей середньоцінового рівня ми використовуємо недорогу, але міцну деревину, яка після ретельної обробки набуває урочистого вигляду, що відповідає траурному заходу. Внутрішня оббивка виконується за допомогою недорогого, але якісного та приємного на вигляд текстилю.</p>    
-                <p className="description" style={{marginBottom: '20px' }}><b>Матеріал:</b> дерев’яна заготовка, з елементами дерева, покритого лаком</p>
-                
-                <ul className={s.coffins__pictureList}>
-                    {CoffinsPictureAveragePriceList.map((image) => (
-                    <li className={s.coffins__pictureListItem} key={image.id}>
-                        <img className={s.coffinsPictureAveragePriceList__img} src={image.src} alt={image.alt} onClick={() => openModal(image.src, image.alt)}/>
-                    </li>
-                    ))}
-                </ul>
+            <CoffinsPictureAveragePriceList />
                     
                 <h2 className="title" style={{ marginBottom: '20px' }}>Економ труни</h2>
                 <p className="description" style={{marginBottom: '20px' }}>Труна не обов’язково повинна бути елітною з цінних порід дерева з багатою внутрішньою оббивкою вишуканими тканинами. Це може бути і зовсім недорога труна. При цьому його скромне, але урочисте оформлення недорогими тканинами, що відповідають жалобній церемонії, підкреслить всю повноту поваги до покійного.</p>    
