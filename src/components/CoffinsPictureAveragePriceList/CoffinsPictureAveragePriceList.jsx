58a68ef420fdd24b192372151c21a13e63ce8302
myusername <chyharov@gmail.com>
2023-10-17 18:53:55 +0300

    ADDED: modal for CoffinsPictureAveragePriceList

--- src/components/CoffinsPictureAveragePriceList/CoffinsPictureAveragePriceList.jsx
+++ src/components/CoffinsPictureAveragePriceList/CoffinsPictureAveragePriceList.jsx
@@ -1,38 +1,32 @@
 import React, { useState } from 'react';
-import Modal from "../CarouselModal/CarouselModal"
+import Modal from '../ModalWindow/ModalWindow'
 import s from './CoffinsPictureAveragePriceList.module.scss'
 
     const CoffinsPictureAveragePrice = [
-        { id: 1, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3365.webp'), alt: 'Зображення 1', width: '180', height: '120' },
-        { id: 2, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3370.webp'), alt: 'Зображення 2', width: '180', height: '120' },
-        { id: 3, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3374.webp'), alt: 'Зображення 3', width: '180', height: '120' },
-        { id: 4, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3375.webp'), alt: 'Зображення 4', width: '180', height: '120' },
-        { id: 5, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3378.webp'), alt: 'Зображення 5', width: '180', height: '120' },
-        { id: 6, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3379.webp'), alt: 'Зображення 6', width: '180', height: '120' },
-        { id: 7, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3384.webp'), alt: 'Зображення 7', width: '180', height: '120' },
-        { id: 8, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3388.webp'), alt: 'Зображення 8', width: '180', height: '120' },
-        { id: 9, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3390.webp'), alt: 'Зображення 9', width: '180', height: '120' },
-        { id: 10, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3395.webp'), alt: 'Зображення 10', width: '180', height: '120' },
-        { id: 11, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3399.webp'), alt: 'Зображення 11', width: '180', height: '120' },
-        { id: 12, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3383.webp'), alt: 'Зображення 12', width: '180', height: '120' },    
+        { id: 1, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3365.webp'), alt: 'Зображення 1' },
+        { id: 2, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3370.webp'), alt: 'Зображення 2' },
+        { id: 3, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3374.webp'), alt: 'Зображення 3' },
+        { id: 4, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3375.webp'), alt: 'Зображення 4' },
+        { id: 5, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3378.webp'), alt: 'Зображення 5' },
+        { id: 6, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3379.webp'), alt: 'Зображення 6' },
+        { id: 7, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3384.webp'), alt: 'Зображення 7' },
+        { id: 8, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3388.webp'), alt: 'Зображення 8' },
+        { id: 9, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3390.webp'), alt: 'Зображення 9' },
+        { id: 10, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3395.webp'), alt: 'Зображення 10' },
+        { id: 11, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3399.webp'), alt: 'Зображення 11' },
+        { id: 12, src: require('../../images/coffinsAveragePrice/coffinsAveragePrice3383.webp'), alt: 'Зображення 12' },    
   ];
 
 const CoffinsPictureAveragePriceList = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
 
-  const [modalImage, setModalImage] = useState(null);
-  const [isOpen, setIsOpen] = useState(false);
-
-  const openModal = (image) => {
-    setModalImage(image);
-    setIsOpen(true);
+  const openModal = (id) => {
+    setSelectedImage(CoffinsPictureAveragePrice.find(image => image.id === id));
+    setModalOpen(true);
     document.body.style.overflow = 'hidden';
   };
 
-  const closeModal = () => {
-    setModalImage(null);
-    setIsOpen(false);
-    document.body.style.overflow = 'auto';
-  };
 
   return (
         <section className={s.sectionCoffinsPictureAveragePriceWreaths}>
@@ -43,23 +37,21 @@ const CoffinsPictureAveragePriceList = () => {
                 <ul className={s.coffins__pictureList}>
                     {CoffinsPictureAveragePrice.map((image) => (
                     <li key={image.id}>
-                        <img className={s.coffinsPictureElitePriceList__img} src={image.src}
-                        alt={image.alt}
-                        onClick={() => openModal(image)}
-                      />
+                        <img className={s.funeralVasesPictureList_img} id={image.id} src={image.src} alt={image.alt} onClick={() => openModal(image.id, image.src, image.alt)}/>
                     </li>
                     ))}
                 </ul>
       
-            {isOpen && modalImage && (
-            <Modal
-              closeModal={closeModal}
-              currentSlide={CoffinsPictureAveragePrice.indexOf(modalImage)}
-              arrayPhotos={CoffinsPictureAveragePrice}
-            />
-            )}
+      {modalOpen && selectedImage && (
+        <Modal
+          selectedImage={selectedImage}
+          arrayPhoto={CoffinsPictureAveragePrice}
+          setSelectedImage={setSelectedImage}
+          setModalOpen={setModalOpen}
+        />
+      )} 
     
-        </section>
+      </section>
     );
   };
 
