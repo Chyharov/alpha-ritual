b26ea8d0af244a27a778b3caeb1b4cbf88e13979
myusername <chyharov@gmail.com>
2024-01-11 23:52:31 +0200

    CHANEGE: markup and styles on section Hero for redesign mobile

--- src/components/SectionHero/SectionHero.jsx
+++ src/components/SectionHero/SectionHero.jsx
@@ -3,18 +3,17 @@ import s from '../SectionHero/SectionHero.module.scss';
 const SectionHero = () => {
   return (
     <section className={s.sectionHero} id='main'>
-      <div className="container">
-        <div className={s.hero__container}>
-          <h1 className="title" style={{ marginBottom: '20px' }}>Склад-магазин ритуальної атрибутики</h1>
-          <a
-            href="https://goo.gl/maps/f8b4aQGY6j4d34Gu8"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <p className={s.hero__address}>
-              м.Київ, вул. Колоскова 9 (район Совського кладовища)
-            </p>
-          </a>
+      <div className={s.heroImg__container}>
+        <div className={'container'}>
+          <div className={s.hero__container}>
+            <h1 style={{ marginTop: '40px' }} className='titleHero'>АЛЬФА-РИТУАЛ</h1>
+            <h2 style={{ marginBottom: '56px' }} className='titleForHero'>поховальний дім</h2>
+          </div>
+          
+          <p style={{ padding: '8px', marginBottom: '40px' }} className='description'>Поховальний дім Альфа має у своєму розпорядженні всі необхідні товари та послуги, що дозволяють проводити поховання або кремацію будь-якого рівня.</p>
+        
+          
+          <button type='button' className={'descriptionBtn ' + s.getConsultation}>Отримати консультацію</button>
         </div>
       </div>
     </section>
