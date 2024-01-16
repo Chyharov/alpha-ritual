fde667d15115cc49996a04bb7088f0d57dfeda00
myusername <chyharov@gmail.com>
2024-01-16 22:34:04 +0200

    CHANGE: style for section Hero

--- src/components/SectionHero/SectionHero.jsx
+++ src/components/SectionHero/SectionHero.jsx
@@ -6,14 +6,14 @@ const SectionHero = () => {
         <div className={s.bgimg}>
       <div className={'container ' + s.heroImg__container}>
           <div className={s.hero__container}>
-            <h1 style={{ marginTop: '40px' }} className='title'>АЛЬФА-РИТУАЛ</h1>
-            <h2 style={{ marginBottom: '56px' }} className='title'>поховальний дім</h2>
+            <h1 style={{ marginTop: '40px' }} className={s.titleHero}>АЛЬФА-РИТУАЛ</h1>
+            <h2 style={{ marginBottom: '56px' }} className={s.titleForHero}>поховальний дім</h2>
           </div>
           
           <p style={{ padding: '8px', marginBottom: '40px' }} className='description'>Поховальний дім Альфа має у своєму розпорядженні всі необхідні товари та послуги, що дозволяють проводити поховання або кремацію будь-якого рівня.</p>
         
           
-          <button type='button' className={'descriptionBtn ' + s.getConsultation}>Отримати консультацію</button>
+          <button type='button' className={s.getConsultation}>Отримати консультацію</button>
         </div>
       </div>
     </section>
