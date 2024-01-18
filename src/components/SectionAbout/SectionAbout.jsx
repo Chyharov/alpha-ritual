4df2a90756d29ab8f2bcb81322dabcc9a75065e7
myusername <chyharov@gmail.com>
2024-01-18 23:24:28 +0200

    UPDATE: SectionAbout

--- src/components/SectionAbout/SectionAbout.jsx
+++ src/components/SectionAbout/SectionAbout.jsx
@@ -11,7 +11,7 @@ const SectionAbout = () => {
           </svg>
           <h1 className={s.title__about}>АЛЬФА-РИТУАЛ</h1>
           <p className="description" style={{ textAlign: 'center', marginBottom: '20px' }}>похоронний дім</p>
-          <p className={s.about__company}>Похоронний дім Альфа має у своєму розпорядженні всі необхідні товари та послуги, що дозволяють проводити поховання або кремацію будь-якого рівня.</p>
+          <p className={s.about__company}> Похоронний дім Альфа має у своєму розпорядженні всі необхідні товари та послуги, що дозволяють проводити поховання або кремацію будь-якого рівня.</p>
         </div>
       </div>
     </section>
