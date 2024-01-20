6bc3f428d125c9b9cd3d0104a2269e7ab2cc5f68
myusername <chyharov@gmail.com>
2024-01-21 00:39:20 +0200

    ADDED: button moreDetails for SectionRitualServices

--- src/components/SectionRitualServices/SectionRitualServices.jsx
+++ src/components/SectionRitualServices/SectionRitualServices.jsx
@@ -2,14 +2,14 @@ import s from '../SectionRitualServices/SectionRitualServices.module.scss';
 
 const SectionRitualServices = () => {
   return (
-    <section className={s.sectionRitualServices} id='services'>
-      <div className="container">
-        <div className={s.ritualServices__container}>
-          <h2 className="title" style={{ marginBottom: '20px', textAlign: 'center' }}>Ритуальні товари</h2>
+    <section className={s.sectionRitualServices}>
+      <div className={`container ${s.ritualServicesContainer}`}>
 
-          <p className="secondaryDescription" style={{ marginBottom: '20px', padding: '8px' }}>Поховальний дім "Альфа-ритуал" - наші фахівці допоможуть вам у важкі моменти життя. Ми розуміємо, наскільки важливо забезпечити гідне та шанобливе прощання з вашими близькими. Наша компанія готова надати вам широкий спектр ритуальних послуг та товарів, щоб зробити цей процес якомога менш тяжким.</p>
+          <h2 className='title' style={{ marginBottom: '24px', textAlign: 'center' }}>Ритуальні товари</h2>
+
+          <p className='secondaryDescription' style={{ marginBottom: '24px', padding: '8px' }}>Поховальний дім 'Альфа-ритуал' - наші фахівці допоможуть вам у важкі моменти життя. Ми розуміємо, наскільки важливо забезпечити гідне та шанобливе прощання з вашими близькими. Наша компанія готова надати вам широкий спектр ритуальних послуг та товарів, щоб зробити цей процес якомога менш тяжким.</p>
           
-        </div>
+          <button type='button' className={s.buttonMoreDetails}>Детальніше</button>
 
       </div>
     </section>
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,6 +1,5 @@
 import Header from 'components/Header/Header';
 import Hero from 'components/SectionHero/SectionHero';
-import SectionTeam from 'components/SectionTeam/SectionTeam';
 import SectionRitualServices from 'components/SectionRitualServices/SectionRitualServices';
 import SectionServicesList from 'components/SectionServicesList/SectionServicesList';
 import Footer from 'components/Footer/Footer';
@@ -11,7 +10,6 @@ const MainPage = () => {
       <Header />
       <main>
         <Hero />
-        <SectionTeam />
         <SectionRitualServices />
         <SectionServicesList />
       </main>
