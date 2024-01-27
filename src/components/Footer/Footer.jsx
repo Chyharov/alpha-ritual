b5310b6f1026efcaee5753bcba7d7e51d004173b
myusername <chyharov@gmail.com>
2024-01-27 23:56:10 +0200

    ADDED: address on footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -21,6 +21,22 @@ const Footer = () => {
               <Link className={s.footerNavListLink}><li style={{ padding: '4px 8px' }} className={s.footerNavListItem}>Додаткові послуги</li></Link>
             </ul>
           </nav>
+
+          <h2>Адреса</h2>
+          <address>
+            <ul>
+              <a className={s.footerNavListLink} href="https://maps.app.goo.gl/RmyTBg28Podhhuek6" target="_blank" rel="noopener noreferrer">
+                <li className={s.footerNavListItem}>
+                  Київ, вул. Івана Федорова, 33
+                </li>
+              </a>
+              <a className={s.footerNavListLink} href="https://maps.app.goo.gl/AwovKjfD7p53WbF56" target="_blank" rel="noopener noreferrer">
+                <li className={s.footerNavListItem}>
+                  м.Київ, вул. Колоскова 9 (район Совського кладовища)
+                </li>
+              </a>
+            </ul>
+          </address>
             <ButtonGetConsultation />
         </div>
       </div>
