a240949b0b3eb0b13904b91f7e9b443a2e4f942d
myusername <chyharov@gmail.com>
2024-02-02 01:14:28 +0200

    ADDED: const title and backgroundImg on AdditionalServicesPage

--- src/components/SectionTitleForPage/SectionTitleForPage.jsx
+++ src/components/SectionTitleForPage/SectionTitleForPage.jsx
@@ -2,19 +2,14 @@ import { Link } from 'react-router-dom';
 import s from './SectionTitleForPage.module.scss';
 import Line from '../../images/TitleForPage/Line6.svg'
 
-
-const SectionTitleForPage = () => {
+const SectionTitleForPage = ({title, backgroundImg}) => {
 
   return (
-    <section className={s.sectionTitleForPage}>
-        <div className='container'>
-            <div className={s.titleForPage__container}>
-                <div className={s.titleContainer}><Link to="/" className={s.linkTitleForPage}>Головна</Link><img className={s.titleLine} src={Line} alt="line" /><p className={s.descriptionTitleForPage}>Додаткові послуги</p></div>
-                
-              
-                <h2 className={s.mainTitle}>Додаткові послуги</h2>
+   <section style={{ backgroundImage: `url(${backgroundImg})` }} className={s.sectionTitleForPage}>
+            <div className='container'>
+                <div className={s.titleContainer}><Link to="/" className={s.linkTitleForPage}>Головна</Link><img className={s.titleLine} src={Line} alt="line" /><p className={s.descriptionTitleForPage}>{title}</p></div>
+                <h2 className={s.mainTitle}>{title}</h2>
             </div>
-      </div>
     </section>
   );
 };
--- src/pages/AdditionalServicesPage/AdditionalServicesPage.jsx
+++ src/pages/AdditionalServicesPage/AdditionalServicesPage.jsx
@@ -1,15 +1,18 @@
 import Header from '../../components/Header/Header'
-import SectionFullAdditionalServices from 'components/SectionFullAdditionalServices/SectionFullAdditionalServices';
-import SectionTitleForPage from 'components/SectionTitleForPage/SectionTitleForPage';
-import Footer from 'components/Footer/Footer';
+import SectionFullAdditionalServices from '../../components/SectionFullAdditionalServices/SectionFullAdditionalServices';
+import SectionTitleForPage from '../../components/SectionTitleForPage/SectionTitleForPage';
+import Footer from '../../components/Footer/Footer';
+
+const title = 'Додаткові послуги';
+const backgroundImg = '/alpha-ritual/static/media/backgroundAdditionalServicesMobile.80b092d15989be58fdcf.jpg';
 
 const AdditionalServicesPage = () => {
   return (
     <>
       <Header />
         <main>
-            <SectionTitleForPage />
-            <SectionFullAdditionalServices />    
+          <SectionTitleForPage title={title} backgroundImg={backgroundImg} />
+          <SectionFullAdditionalServices />    
         </main>
       <Footer />
     </>
