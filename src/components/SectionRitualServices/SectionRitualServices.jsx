e4587d96197f2a3543adce00409044ca3117f041
myusername <chyharov@gmail.com>
2024-02-04 16:10:58 +0200

    UPDATE: buttons on SectionRitualServices and SectionRitualTransport

--- src/components/SectionRitualServices/SectionRitualServices.jsx
+++ src/components/SectionRitualServices/SectionRitualServices.jsx
@@ -17,7 +17,7 @@ const SectionRitualServices = () => {
         <h2 className='title' style={{ marginBottom: '24px', textAlign: 'center' }}>Ритуальні товари</h2>
 
         {displayedServices.map((item) => (
-          <p className='secondaryDescription' style={{ marginBottom: '24px', padding: '8px' }}>Поховальний дім 'Альфа-ритуал' - наші фахівці допоможуть вам у важкі моменти життя. Ми розуміємо, наскільки важливо забезпечити гідне та шанобливе прощання з вашими близькими. Наша компанія готова надати вам широкий спектр ритуальних послуг та товарів, щоб зробити цей процес якомога менш тяжким.</p>
+            <p className='secondaryDescription' style={{ marginBottom: '24px', padding: '8px' }}>{item.title}</p>
         ))}
 
         {!showAll && (
--- src/components/SectionRitualTransport/SectionRitualTransport.jsx
+++ src/components/SectionRitualTransport/SectionRitualTransport.jsx
@@ -1,16 +1,29 @@
+import React, { useState } from 'react';
 import s from './SectionRitualTransport.module.scss';
 
+const ritualTransportDescriptionArray = [
+  { id: 1, title: "Саме слово «катафалк» буквально означає постамент для встановлення труни з покійним під час поховання. Однак у нашій мові воно набуло дещо іншого значення – транспорт для ритуального перевезення покійного під час поховальної церемонії." },
+  { id: 2, title: "Померлих військових та монарших осіб перевозили під час похорону на гарматних лафетах або запряжених у кінні упряжки возах, прикрашених квітами. Сьогодні функції ритуального транспорту виконують автомобілі катафалки. Це можуть бути як легкові машини із задніми дверима, через які завантажується труна, так і місткі мікро- або повноцінні автобуси з можливістю посадки рідних та близьких покійного." },
+  { id: 3, title: "У ритуальному бюро поховальний дім «Альфа» ви можете замовити у будь-яку точку Києва та Київської області один із чотирьох видів катафалків. Ціна таких послуг залежить від класу ритуального транспорту та його місткості. Це може бути як бюджетний економ варіант, так і катафалк VIP-рівня." },
+];
+
 const SectionRitualTransport = () => {
+  const [showAll, setShowAll] = useState(false);
+  const displayedServices = showAll ? ritualTransportDescriptionArray : ritualTransportDescriptionArray.slice(0, 1);
+
   return (
     <section className={s.sectionRitualTransport}>
       <div className={`container ${s.ritualServicesContainer}`}>
 
           <h2 className='title' style={{ marginBottom: '24px', textAlign: 'center' }}>Ритуальний транспорт</h2>
-
-          <p className='secondaryDescription' style={{ marginBottom: '24px', padding: '8px' }}>Саме слово «катафалк» буквально означає постамент для встановлення труни з покійним під час поховання. Однак у нашій мові воно набуло дещо іншого значення – транспорт для ритуального перевезення покійного під час поховальної церемонії.</p>
           
-          <button type='button' className={s.buttonMoreDetails}>Детальніше</button>
-
+          {displayedServices.map((item) => (
+            <p className='secondaryDescription' style={{ marginBottom: '24px', padding: '8px' }}>{item.title}</p>
+          ))}
+        
+          {!showAll && (
+            <button type='button' className={s.buttonMoreDetails} onClick={() => setShowAll(true)}>Детальніше</button>
+          )}
       </div>
     </section>
   );
