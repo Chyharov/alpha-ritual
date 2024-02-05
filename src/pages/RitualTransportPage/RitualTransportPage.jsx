d764203e6baea8c8e49a1bdb3fd4584beeaa2622
myusername <chyharov@gmail.com>
2024-02-06 00:10:48 +0200

    ADDED: SectionTitleForPage on RitualTransportPage

--- src/pages/RitualTransportPage/RitualTransportPage.jsx
+++ src/pages/RitualTransportPage/RitualTransportPage.jsx
@@ -1,12 +1,16 @@
 import Header from 'components/Header/Header';
 import Footer from 'components/Footer/Footer';
+import SectionTitleForPage from '../../components/SectionTitleForPage/SectionTitleForPage';
+import backgroundImg from '../../images/additionalServices/backgroundAdditionalServicesMob.jpg'
+
+const title = 'Ритуальний транспорт';
 
 const RitualTransportPage = () => {
   return (
     <>
       <Header />
         <main>
-
+          <SectionTitleForPage title={title} backgroundImg={backgroundImg} />
         </main>
       <Footer />
     </>
