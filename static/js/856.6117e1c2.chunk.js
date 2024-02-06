8b51a2f47e1e4575f08993fe7c369517edbf4c9d
Chyharov <102535915+Chyharov@users.noreply.github.com>
2024-02-06 21:56:14 +0000

    Deploying to gh-pages from @ Chyharov/alpha-ritual@de5ae880e633f639068c1bb8be31959994106697 🚀

--- static/js/856.6117e1c2.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkalpha_ritual=self.webpackChunkalpha_ritual||[]).push([[856],{6856:function(t,a,n){n.d(a,{oTp:function(){return r}});var l=n(9983);function r(t){return(0,l.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0V0z",opacity:".87"}},{tag:"path",attr:{d:"M17.51 3.87L15.73 2.1 5.84 12l9.9 9.9 1.77-1.77L9.38 12l8.13-8.13z"}}]})(t)}}}]);
-//# sourceMappingURL=856.6117e1c2.chunk.js.map
