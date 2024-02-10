d8c9eabc43f4029ce66ed11936b45ae57e87b87b
myusername <chyharov@gmail.com>
2024-02-11 01:46:49 +0200

    ADDED: SectionRitualServicesList

--- src/components/SectionRitualServicesList/SectionRitualServicesList.jsx
+++ src/components/SectionRitualServicesList/SectionRitualServicesList.jsx
@@ -1,30 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ButtonGetConsultation from 'components/ButtonGetConsultation/ButtonGetConsultation';
 import s from './SectionRitualServicesList.module.scss';
 
 const additionalServices = [
-  { id: 1, title: 'Вінки', link: '/wreaths' },
-  { id: 2, title: 'Труни', link: '/coffins' },
-  { id: 3, title: 'Хрести', link: '/crosses' },
-  { id: 4, title: 'Покривала', link: '/covered' },
-  { id: 5, title: 'Урни для праху', link: '/vases' },
-  { id: 6, title: 'Декор', link: '/decorations' },
-  { id: 7, title: 'Інша атрибутика', link: '/otherattributes' },
+    { id: 1, title: 'Економ', price: 'Уточнюйте', description: ['Труна дерев’яна (оббита тканиною)', 'Поховальний набір', 'Покривало', 'Вінок із штучних квітів (2 шт)', 'Хрест на могилу з табличкою', 'Автокатафалк (15-17 посадочних місць)', 'Поховальна бригада (4 особи)'] },
 ];
 
 const SectionRitualServicesList = () => {
+  const [selectedService, setSelectedService] = useState(null);
+
+  const toggleDetails = (id) => {
+    if (selectedService === id) {
+      setSelectedService(null);
+    } else {
+      setSelectedService(id);
+    }
+  };
 
   return (
    <section className={s.sectionRitualServicesList}>
         <div className={'container ' + s.ritualServicesContainer}>
-
-        <h2 className='title' style={{ marginBottom: '24px' }}>Додаткові послуги</h2>
-
+        <h2>Поховання</h2>
         <ul className={s.ritualServices__list}>
           {additionalServices.map((item) => (
             <li className={s.ritualServices__listItem} key={item.id}>
               <div className={s.ritualServices__border}>
-                <h2 className={s.ritualServices__title}>{item.title}</h2>
-                <button className={s.ritualServices__link} to={item.link}>Деталі</button>
+                {!selectedService && <h2 className={s.ritualServices__title}>{item.title}</h2>}
+                {selectedService === item.id && (
+                  <div className={s.ritualServices__ContainerInfo}>
+                    <h2>{item.title}</h2>
+                    <p className={s.ritualServices__price}>{item.price}</p>
+                    <ul>
+                      {item.description.map((desc, index) => (
+                        <li key={index}>{desc}</li>
+                      ))}
+                    </ul>
+                    <ButtonGetConsultation />
+                  </div>
+                )}
+                {!selectedService && (
+                  <button className={s.ritualServices__btnDetails} onClick={() => toggleDetails(item.id)}>Деталі</button>
+                )}
               </div>
             </li>
           ))}
