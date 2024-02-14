dd97fd9d05e2d8f0d7b874692b0eab3087f4e963
myusername <chyharov@gmail.com>
2024-02-14 23:47:50 +0200

    UPDATE: Photo for BlackMercedes

--- src/components/BlackMercedesGallery/BlackMercedesGallery.jsx
+++ src/components/BlackMercedesGallery/BlackMercedesGallery.jsx
@@ -7,12 +7,18 @@ import arrowRight from '../../images/arrowRight.svg';
 import s from './BlackMercedesGallery.module.scss';
 
 const RitualCarParBlackMercedesPhoto = [
-  { id: 1, src: require('../../images/testBlackMercedes/testBlackMercedes1.webp'), alt: 'Зображення 1', width: '280', height: '210' },
-  { id: 2, src: require('../../images/testBlackMercedes/testBlackMercedes2.webp'), alt: 'Зображення 2', width: '280', height: '210' },
-  { id: 3, src: require('../../images/testBlackMercedes/testBlackMercedes3.webp'), alt: 'Зображення 3', width: '150', height: '150' },
-  { id: 4, src: require('../../images/testBlackMercedes/testBlackMercedes4.webp'), alt: 'Зображення 4', width: '280', height: '210' },
-  { id: 5, src: require('../../images/testBlackMercedes/testBlackMercedes5.webp'), alt: 'Зображення 5', width: '280', height: '210' },
-  { id: 6, src: require('../../images/testBlackMercedes/testBlackMercedes6.webp'), alt: 'Зображення 6', width: '280', height: '210' },
+  { id: 1, src: require('../../images/blackMercedes/blackMercedes1.webp'), alt: 'Зображення 1', width: '280', height: '210' },
+  { id: 2, src: require('../../images/blackMercedes/blackMercedes2.webp'), alt: 'Зображення 2', width: '280', height: '210' },
+  { id: 3, src: require('../../images/blackMercedes/blackMercedes3.webp'), alt: 'Зображення 3', width: '150', height: '150' },
+  { id: 4, src: require('../../images/blackMercedes/blackMercedes4.webp'), alt: 'Зображення 4', width: '280', height: '210' },
+  { id: 5, src: require('../../images/blackMercedes/blackMercedes5.webp'), alt: 'Зображення 5', width: '280', height: '210' },
+  { id: 6, src: require('../../images/blackMercedes/blackMercedes6.webp'), alt: 'Зображення 6', width: '280', height: '210' },
+  { id: 7, src: require('../../images/blackMercedes/blackMercedes7.webp'), alt: 'Зображення 7', width: '280', height: '210' },
+  { id: 8, src: require('../../images/blackMercedes/blackMercedes8.webp'), alt: 'Зображення 8', width: '280', height: '210' },
+  { id: 9, src: require('../../images/blackMercedes/blackMercedes9.webp'), alt: 'Зображення 9', width: '280', height: '210' },
+  { id: 10, src: require('../../images/blackMercedes/blackMercedes10.webp'), alt: 'Зображення 10', width: '280', height: '210' },
+  { id: 11, src: require('../../images/blackMercedes/blackMercedes11.webp'), alt: 'Зображення 11', width: '280', height: '210' },
+  { id: 12, src: require('../../images/blackMercedes/blackMercedes12.webp'), alt: 'Зображення 12', width: '280', height: '210' }
 ];
 
 const BlackMercedes = () => {
