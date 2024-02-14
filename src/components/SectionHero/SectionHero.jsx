4ac9bb9fa1a0f1d9319fc42e36de68ed5f73af32
myusername <chyharov@gmail.com>
2024-02-14 23:50:30 +0200

    UPDATE: text on SectionHero

--- src/components/SectionHero/SectionHero.jsx
+++ src/components/SectionHero/SectionHero.jsx
@@ -8,10 +8,10 @@ const SectionHero = () => {
       <div className={'container ' + s.heroImg__container}>
           <div className={s.hero__container}>
             <h1 style={{ marginTop: '40px' }} className={s.titleHero}>АЛЬФА-РИТУАЛ</h1>
-            <h2 style={{ marginBottom: '56px' }} className={s.titleForHero}>поховальний дім</h2>
+            <h2 style={{ marginBottom: '56px' }} className={s.titleForHero}>Похоронний дім</h2>
           </div>
           
-          <p style={{ padding: '8px', marginBottom: '40px' }} className='description'>Поховальний дім Альфа має у своєму розпорядженні всі необхідні товари та послуги, що дозволяють проводити поховання або кремацію будь-якого рівня.</p>
+          <p style={{ padding: '8px', marginBottom: '40px' }} className='description'>Похоронний дім Альфа має у своєму розпорядженні всі необхідні товари та послуги, що дозволяють проводити поховання або кремацію будь-якого рівня.</p>
         
           <ButtonGetConsultation />
           
