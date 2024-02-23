b9c3cae4a93e0bbe101210ff79b7cb18e5b11f62
myusername <chyharov@gmail.com>
2024-02-23 19:13:50 +0200

    ADDED: SectionTitleForPage and Footer for freshFlowersWreathPage

--- src/pages/FreshFlowersWreathPage/FreshFlowersWreathPage.jsx
+++ src/pages/FreshFlowersWreathPage/FreshFlowersWreathPage.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
 import Header from '../../components/Header/Header'
+import SectionTitleForPage from '../../components/SectionTitleForPage/SectionTitleForPage';
+import backgroundImg from '../../images/freshFlowersWreath/freshFlowersWreathBackgroundMob.jpg'
 import SectionFreshFlowersWreath from 'components/SectionFreshFlowersWreath/SectionFreshFlowersWreath';
+import Footer from 'components/Footer/Footer';
+
+const title = 'Вінок із живих квітів';
 
 const freshFlowersWreathPage = () => {
 
     return (
       <>
         <Header />
-        <main>
-          <SectionFreshFlowersWreath />
-        </main>
-        
+          <main>
+            <SectionTitleForPage title={title} backgroundImg={backgroundImg} />
+            <SectionFreshFlowersWreath />
+          </main>
+        <Footer />
       </>
     );
   };
