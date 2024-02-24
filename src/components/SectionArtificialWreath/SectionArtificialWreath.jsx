e527612095d1c9055ecf8b453e4b32bff5969f57
myusername <chyharov@gmail.com>
2024-02-25 00:48:41 +0200

    UPDATE: photos on ArtificialWreathsPictureList for SectionArtificialWreath

--- src/components/SectionArtificialWreath/SectionArtificialWreath.jsx
+++ src/components/SectionArtificialWreath/SectionArtificialWreath.jsx
@@ -7,7 +7,23 @@ import arrowRight from '../../images/arrowRight.svg';
 import s from './SectionArtificialWreath.module.scss'
 
 const ArtificialWreathsPictureList = [
-  { id: 1, src: require('../../images/artificialWreaths/artificialWreaths3492.webp'), alt: 'Зображення 1', title: '210см' },
+  { id: 1, src: require('../../images/artificialWreaths/artificialWreaths1.jpg'), alt: 'Зображення 1' },
+  { id: 2, src: require('../../images/artificialWreaths/artificialWreaths2.jpg'), alt: 'Зображення 2' },
+  { id: 3, src: require('../../images/artificialWreaths/artificialWreaths3.jpg'), alt: 'Зображення 3' },
+  { id: 4, src: require('../../images/artificialWreaths/artificialWreaths4.jpg'), alt: 'Зображення 4' },
+  { id: 5, src: require('../../images/artificialWreaths/artificialWreaths5.jpg'), alt: 'Зображення 5' },
+  { id: 6, src: require('../../images/artificialWreaths/artificialWreaths6.jpg'), alt: 'Зображення 6' },
+  { id: 7, src: require('../../images/artificialWreaths/artificialWreaths7.jpg'), alt: 'Зображення 7' },
+  { id: 8, src: require('../../images/artificialWreaths/artificialWreaths8.jpg'), alt: 'Зображення 8' },
+  { id: 9, src: require('../../images/artificialWreaths/artificialWreaths9.jpg'), alt: 'Зображення 9' },
+  { id: 10, src: require('../../images/artificialWreaths/artificialWreaths10.jpg'), alt: 'Зображення 10' },
+  { id: 11, src: require('../../images/artificialWreaths/artificialWreaths11.jpg'), alt: 'Зображення 11' },
+  { id: 12, src: require('../../images/artificialWreaths/artificialWreaths12.jpg'), alt: 'Зображення 12' },
+  { id: 13, src: require('../../images/artificialWreaths/artificialWreaths13.jpg'), alt: 'Зображення 13' },
+  { id: 14, src: require('../../images/artificialWreaths/artificialWreaths14.jpg'), alt: 'Зображення 14' },
+  { id: 15, src: require('../../images/artificialWreaths/artificialWreaths15.jpg'), alt: 'Зображення 15' },
+  { id: 16, src: require('../../images/artificialWreaths/artificialWreaths16.jpg'), alt: 'Зображення 16' },
+  { id: 17, src: require('../../images/artificialWreaths/artificialWreaths17.jpg'), alt: 'Зображення 17' },
 ];
 
 const formCompositionForArtificialWreath = [
