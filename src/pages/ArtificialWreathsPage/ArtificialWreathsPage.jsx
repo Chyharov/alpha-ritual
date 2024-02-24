c639c8f5e632b15f2f2397ffbbc0921591b59ec6
myusername <chyharov@gmail.com>
2024-02-24 23:36:13 +0200

    ADDED: SectionTitleForPage and Footer on ArtificialWreathsPage

--- src/pages/ArtificialWreathsPage/ArtificialWreathsPage.jsx
+++ src/pages/ArtificialWreathsPage/ArtificialWreathsPage.jsx
@@ -1,14 +1,20 @@
 import Header from '../../components/Header/Header'
 import SectionArtificialWreath from '../../components/SectionArtificialWreath/SectionArtificialWreath'
+import SectionTitleForPage from 'components/SectionTitleForPage/SectionTitleForPage';
+import backgroundImg from '../../images/artificialWreaths/backgroundArtificalWreathsMob.jpg'
+import Footer from 'components/Footer/Footer';
+
+const title = 'Вінок із штучних квітів';
 
 const ArtificialWreathsPage = () => {
   return (
     <>
       <Header />
-      <main>
-        <SectionArtificialWreath/>
-      </main>
-      
+        <main>
+          <SectionTitleForPage title={title} backgroundImg={backgroundImg} />
+          <SectionArtificialWreath/>
+        </main>
+      <Footer />
     </>
   );
 };
--- src/pages/RitualServicesPage/RitualServicesPage.jsx
+++ src/pages/RitualServicesPage/RitualServicesPage.jsx
@@ -1,8 +1,8 @@
 import Header from 'components/Header/Header';
 import SectionTitleForPage from '../../components/SectionTitleForPage/SectionTitleForPage';
+import backgroundImg from '../../images/RitualServices/backgroundRitualServicesMob.jpg'
 import SectionRitualServicesList from 'components/SectionRitualServicesList/SectionRitualServicesList';
 import Footer from 'components/Footer/Footer';
-import backgroundImg from '../../images/RitualServices/backgroundRitualServicesMob.jpg'
 
 const title = 'Ритуальні послуги';
 
@@ -11,14 +11,12 @@ const RitualServicesPage = () => {
     <>
       <Header />
         <main>
-            <SectionTitleForPage title={title} backgroundImg={backgroundImg} />
-            <SectionRitualServicesList />
+          <SectionTitleForPage title={title} backgroundImg={backgroundImg} />
+          <SectionRitualServicesList />
         </main>
       <Footer />
     </>
   );
 };
-
-
 
 export default RitualServicesPage;
