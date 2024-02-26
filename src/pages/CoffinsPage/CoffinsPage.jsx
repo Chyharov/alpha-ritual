84d99be3d0ad943eda25616e384e6c8204b92293
myusername <chyharov@gmail.com>
2024-02-26 23:29:40 +0200

    ADDED: SectionTitleForPage and Footer on CoffinsPage

--- src/pages/CoffinsPage/CoffinsPage.jsx
+++ src/pages/CoffinsPage/CoffinsPage.jsx
@@ -1,14 +1,20 @@
 import Header from '../../components/Header/Header'
+import SectionTitleForPage from '../../components/SectionTitleForPage/SectionTitleForPage';
+import backgroundImg from '../../images/coffins/backgroundCoffinsMob.jpg'
 import SectionCoffins from '../../components/SectionCoffins/SectionCoffins'
+import Footer from 'components/Footer/Footer';
+
+const title = 'Труни';
 
 const CoffinsPage = () => {
   return (
     <>
       <Header />
-      <main>
-        <SectionCoffins />
-      </main>
-
+        <main>
+          <SectionTitleForPage title={title} backgroundImg={backgroundImg} />
+          <SectionCoffins />
+        </main>
+      <Footer />
     </>
   );
 };
