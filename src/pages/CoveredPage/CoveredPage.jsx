bc5a710b46c66b47c42dfe927c1304ad755c11ca
myusername <chyharov@gmail.com>
2024-02-28 23:03:36 +0200

    ADDED: SectionTitleForPage and Footer on CoveredPage

--- src/pages/CoveredPage/CoveredPage.jsx
+++ src/pages/CoveredPage/CoveredPage.jsx
@@ -1,13 +1,20 @@
 import Header from '../../components/Header/Header'
+import SectionTitleForPage from '../../components/SectionTitleForPage/SectionTitleForPage';
+import backgroundImg from '../../images/RitualServices/backgroundRitualServicesMob.jpg'
 import SectionCoverd from 'components/SectionCovered/SectionCovered';
+import Footer from 'components/Footer/Footer';
+
+const title = 'Покривала';
 
 const CoveredPage = () => {
   return (
     <>
       <Header />
       <main>
+        <SectionTitleForPage title={title} backgroundImg={backgroundImg} />
         <SectionCoverd />
       </main>
+      <Footer />
     </>
   );
 };
