2dee1bec6954168c8efdb031f15b072c69e416ec
myusername <chyharov@gmail.com>
2024-03-06 00:14:27 +0200

    UPDATE: markup and styles on SectionCrosses

--- src/components/SectionCrosses/SectionCrosses.jsx
+++ src/components/SectionCrosses/SectionCrosses.jsx
@@ -1,21 +1,26 @@
 import React, { useState } from "react";
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
+import Modal from '../ModalWindow/ModalWindow'
+import arrowLeft from '../../images/arrowLeft.svg';
+import arrowRight from '../../images/arrowRight.svg';
 import ButtonMoreDetails from 'components/ButtonMoreDetails/ButtonMoreDetails';
 import s from './SectionCrosses.module.scss'
 
-// const CrossesPictureList = [
-//       { id: 1, src: require('../../images/crosses/crosses1.webp'), alt: 'Зображення 1', width: '120', height: '90' },
-//       { id: 2, src: require('../../images/crosses/crosses2.webp'), alt: 'Зображення 2', width: '120', height: '90' },
-//       { id: 3, src: require('../../images/crosses/crosses3.webp'), alt: 'Зображення 3', width: '120', height: '90' },
-//       { id: 4, src: require('../../images/crosses/crosses4.webp'), alt: 'Зображення 4', width: '120', height: '90' },
-//       { id: 5, src: require('../../images/crosses/crosses5.webp'), alt: 'Зображення 5', width: '120', height: '90' },
-//       { id: 6, src: require('../../images/crosses/crosses6.webp'), alt: 'Зображення 6', width: '120', height: '90' },
-//       { id: 7, src: require('../../images/crosses/crosses7.webp'), alt: 'Зображення 7', width: '120', height: '90' },
-//       { id: 8, src: require('../../images/crosses/crosses8.webp'), alt: 'Зображення 8', width: '120', height: '90' },
-//       { id: 9, src: require('../../images/crosses/crosses9.webp'), alt: 'Зображення 9', width: '120', height: '90' },
-//       { id: 10, src: require('../../images/crosses/crosses10.webp'), alt: 'Зображення 10', width: '120', height: '90' },
-//       { id: 11, src: require('../../images/crosses/crosses11.webp'), alt: 'Зображення 11', width: '120', height: '90' },
-//       { id: 12, src: require('../../images/crosses/crosses12.webp'), alt: 'Зображення 12', width: '120', height: '90' },
-// ];
+const CrossesPictureList = [
+      { id: 1, src: require('../../images/crosses/crosses1.webp'), alt: 'Зображення 1', width: '120', height: '90' },
+      { id: 2, src: require('../../images/crosses/crosses2.webp'), alt: 'Зображення 2', width: '120', height: '90' },
+      { id: 3, src: require('../../images/crosses/crosses3.webp'), alt: 'Зображення 3', width: '120', height: '90' },
+      { id: 4, src: require('../../images/crosses/crosses4.webp'), alt: 'Зображення 4', width: '120', height: '90' },
+      { id: 5, src: require('../../images/crosses/crosses5.webp'), alt: 'Зображення 5', width: '120', height: '90' },
+      { id: 6, src: require('../../images/crosses/crosses6.webp'), alt: 'Зображення 6', width: '120', height: '90' },
+      { id: 7, src: require('../../images/crosses/crosses7.webp'), alt: 'Зображення 7', width: '120', height: '90' },
+      { id: 8, src: require('../../images/crosses/crosses8.webp'), alt: 'Зображення 8', width: '120', height: '90' },
+      { id: 9, src: require('../../images/crosses/crosses9.webp'), alt: 'Зображення 9', width: '120', height: '90' },
+      { id: 10, src: require('../../images/crosses/crosses10.webp'), alt: 'Зображення 10', width: '120', height: '90' },
+      { id: 11, src: require('../../images/crosses/crosses11.webp'), alt: 'Зображення 11', width: '120', height: '90' },
+      { id: 12, src: require('../../images/crosses/crosses12.webp'), alt: 'Зображення 12', width: '120', height: '90' },
+];
 
 const whyArtificialWreath = [
   { id: 1, title: "Жодних церковних заборон із цього приводу немає. Справа не в православних канонах, проблема виключно у практичній площині. Вилучення грунту під час копання могильної ями, а потім його повернення в могилу після поховання робить землю спушеною, а значить, об’ємною та нестабільною." },
@@ -33,10 +38,20 @@ const WhichRitualCross = [
   
 const SectionCrosses = () => {
   const buttonDescription = 'Детальніше';
+  const buttonShowAllDescription = 'Переглянути всі';
   const [showWhy, setShowWhy] = useState(false);
   const [showWhich, setShowWhich] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
   const displayedWhyCantInstalMonument = showWhy ? whyArtificialWreath : whyArtificialWreath.slice(0, 1);
   const displayedWhichRitualCross = showWhich ? WhichRitualCross : WhichRitualCross.slice(0, 1);
+
+
+  const openModal = (id) => {
+    setSelectedImage(CrossesPictureList.find(image => image.id === id));
+    setModalOpen(true);
+    document.body.style.overflow = 'hidden';
+  };
 
   return (
     <section className={s.sectionCrosses}>
@@ -77,6 +92,48 @@ const SectionCrosses = () => {
         )}
 
         <h2 className='title' style={{ marginBottom: '16px', textAlign: 'center' }}>Хрести</h2>
+
+        <div className={s.photosContainer}>
+          <CarouselProvider
+            naturalSlideWidth={288}
+            naturalSlideHeight={288}
+            totalSlides={CrossesPictureList.length}
+            infinite={true}>
+            
+            <Slider className={s.sliderPhoto}>
+              {CrossesPictureList.map(photo => (
+                <Slide key={photo.id}>
+                  <img src={photo.src} alt={photo.alt} onClick={() => openModal(photo.id)} />
+                </Slide>
+              ))}
+            </Slider>
+
+            <div className={s.containerForButtons}>
+              <ButtonBack className={s.buttonNavigation}><img src={arrowLeft} alt="arrowLeft" /></ButtonBack>
+              <ButtonNext className={s.buttonNavigation}><img src={arrowRight} alt="arrowRight" /></ButtonNext>
+            </div>
+            </CarouselProvider>
+          </div>
+
+        
+        {modalOpen && selectedImage && (
+          <Modal
+          selectedImage={selectedImage}
+          arrayPhoto={CrossesPictureList}
+          setSelectedImage={setSelectedImage}
+          setModalOpen={setModalOpen}
+          />
+        )}
+
+        <ButtonMoreDetails style={{ marginBottom: '64px' }} buttonDescription={buttonShowAllDescription} />  
+        
+        <h2 className='title' style={{ marginBottom: '16px', textAlign: 'center' }}>Як замовити ритуальний хрест у Києві?</h2>
+
+        <p className="description" style={{ marginBottom: '16px' }}>Поховальне бюро «Альфа» має власні виробничі потужності, які дозволяють виготовляти дерев’яні та металеві ритуальні хрести на могилу. Кожен виріб проходить ретельний контроль на предмет якості деревообробки та столярних робіт у дерев’яних моделях, а також якості металообробки та зварювання у металевих.</p>
+
+        <p className="description" style={{ marginBottom: '16px' }}>У виробництві дерев’яних виробів ми використовуємо як недорогі, і цінні породи дерева. Завдяки цьому ми пропонуємо широку лінійку цін – від найбюджетніших варіантів, які, проте, тривалий час виконують свою практичну та сакральну функцію, так і елітні ритуальні хрести, які можна назвати витвором мистецтва.</p>
+
+        <p className="description">Незалежно від основного матеріалу та цінової категорії, кожен виріб покривається довговічним лаком або фарбою, завдяки яким ритуальний хрест зберігає свій вигляд протягом декількох років в умовах суворих зим та спекотного літа.</p>
 
       </div>
     </section>
