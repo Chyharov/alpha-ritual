74562148fbe3218c4d92cb1476b09f03d0749764
myusername <chyharov@gmail.com>
2024-03-07 22:50:45 +0200

    UPDATE: markup and styles OtherAttributes

--- src/components/OtherAttributes/OtherAttributes.jsx
+++ src/components/OtherAttributes/OtherAttributes.jsx
@@ -1,31 +1,31 @@
 import { Link } from 'react-router-dom';
+import ButtonMoreDetails from 'components/ButtonMoreDetails/ButtonMoreDetails';
 import s from '../OtherAttributes/OtherAttributes.module.scss';
 
 
 const OtherAttributes = () => {
+  const buttonDescription = 'Детальніше';
+
+  
+
   return (
-    <section className={s.sectionOtherAttributes} id='servicesList'>
+    <section className={s.sectionOtherAttributes}>
       <div className={'container ' + s.ritualServices__container}>
 
-        <ul className={s.ritualServices__list}>
-                      
-          <li className={s.ritualServices__listItem}>
-            <Link className={s.ritualServices__link} to="/funeralset">
-              <div className={s.ritualServices__border}>
-                <h2 className={s.ritualServices__title}>Похоронний набір</h2>
-              </div>
-            </Link>
-          </li>  
+        <h2 className="title" style={{ marginBottom: '16px', textAlign: 'center' }}>Поховальний набір</h2>
 
-          <li className={s.ritualServices__listItem}>
-            <Link className={s.ritualServices__link} to="/funeralportrait">
-              <div className={s.ritualServices__border}>
-                <h2 className={s.ritualServices__title}>Портрет на похорон</h2>
-              </div>
-            </Link>
-          </li>      
+        <p className="description" style={{ marginBottom: '24px' }}>Ритуальне приладдя – це невід’ємний атрибут традиційного обряду поховання православного християнина. Без такого поховального набору неможливо провести церковне відспівування покійного. Поховальний будинок «Альфа» бере на себе весь клопіт з організації поховання, у тому числі й у такому делікатному питанні, як дотримання заупокійних церковних обрядів.</p>
 
-        </ul>     
+            <Link className={s.ritualServices__link} to="/funeralset" style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+              <ButtonMoreDetails  buttonDescription={buttonDescription} />
+            </Link>
+ 
+
+
+            {/* <Link className={s.ritualServices__link} to="/funeralportrait" style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+              <ButtonMoreDetails  buttonDescription={buttonDescription} />
+            </Link> */}
+       
       </div>
     </section>
   );
--- src/pages/OtherAttributesPage/OtherAttributesPage.jsx
+++ src/pages/OtherAttributesPage/OtherAttributesPage.jsx
@@ -1,13 +1,24 @@
 import Header from '../../components/Header/Header'
+import SectionTitleForPage from '../../components/SectionTitleForPage/SectionTitleForPage';
+import backgroundImg from '../../images/otherAttributes/OtherAttributesBackgroundMob.jpg';
 import OtherAttributes from 'components/OtherAttributes/OtherAttributes';
+import Footer from 'components/Footer/Footer';
+
+
+const titleProps = {
+  title: 'Вінки',
+  backgroundImg: backgroundImg
+};
 
 const OtherAttributesPage = () => {
   return (
     <>
       <Header />
-      <main>
-        <OtherAttributes />
-      </main>
+        <main>
+          <SectionTitleForPage {...titleProps} />
+          <OtherAttributes />
+        </main>
+      <Footer />
     </>
   );
 };
