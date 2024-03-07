84ddaef2e197816eb962db7213cff97082d4fb6d
myusername <chyharov@gmail.com>
2024-03-07 21:00:53 +0200

    UPDATE: photos for decorationsPhotos on SectionDecorations

--- src/components/SectionDecorations/SectionDecorations.jsx
+++ src/components/SectionDecorations/SectionDecorations.jsx
@@ -4,13 +4,13 @@ import ButtonMoreDetails from 'components/ButtonMoreDetails/ButtonMoreDetails';
 import s from "./SectionDecorations.module.scss"
 
 const decorationsPhotos = [
-  { id: 1, src: require('../../images/decorations/decoration1.webp'), alt: 'Зображення 1', width: '280', height: '210' },
-  { id: 2, src: require('../../images/decorations/decoration2.webp'), alt: 'Зображення 2', width: '280', height: '210' },
-  { id: 3, src: require('../../images/decorations/decoration3.webp'), alt: 'Зображення 3', width: '150', height: '150' },
-  { id: 4, src: require('../../images/decorations/decoration4.webp'), alt: 'Зображення 4', width: '280', height: '210' },
-  { id: 5, src: require('../../images/decorations/decoration5.webp'), alt: 'Зображення 5', width: '280', height: '210' },
-  { id: 6, src: require('../../images/decorations/decoration6.webp'), alt: 'Зображення 6', width: '280', height: '210' },
-  { id: 7, src: require('../../images/decorations/decoration7.webp'), alt: 'Зображення 7', width: '280', height: '210' },
+  { id: 1, src: require('../../images/decorations/decoration1.jpg'), alt: 'Зображення 1', width: '280', height: '210' },
+  { id: 2, src: require('../../images/decorations/decoration2.jpg'), alt: 'Зображення 2', width: '280', height: '210' },
+  { id: 3, src: require('../../images/decorations/decoration3.jpg'), alt: 'Зображення 3', width: '150', height: '150' },
+  { id: 4, src: require('../../images/decorations/decoration4.jpg'), alt: 'Зображення 4', width: '280', height: '210' },
+  { id: 5, src: require('../../images/decorations/decoration5.jpg'), alt: 'Зображення 5', width: '280', height: '210' },
+  { id: 6, src: require('../../images/decorations/decoration6.jpg'), alt: 'Зображення 6', width: '280', height: '210' },
+  { id: 7, src: require('../../images/decorations/decoration7.jpg'), alt: 'Зображення 7', width: '280', height: '210' },
 ];
 
 const buttonShowAll = 'Переглянути всі';
