1b33f31c470c660cf91073e788346dc5172d9b74
myusername <chyharov@gmail.com>
2024-03-08 23:50:59 +0200

    ADDED: SectionTitleForPage and Footer on FuneralSetPage

--- src/pages/FuneralSetPage/FuneralSetPage.jsx
+++ src/pages/FuneralSetPage/FuneralSetPage.jsx
@@ -1,14 +1,23 @@
 import Header from '../../components/Header/Header'
+import SectionTitleForPage from '../../components/SectionTitleForPage/SectionTitleForPage';
+import backgroundImg from '../../images/wreaths/WreathsBackgroundMob.jpg';
 import SectionFuneralSet from 'components/SectionFuneralSet/SectionFuneralSet';
+import Footer from 'components/Footer/Footer';
+
+const titleProps = {
+  title: 'Вінки',
+  backgroundImg: backgroundImg
+};
 
 const FuneralSetPage = () => {
   return (
     <>
       <Header />
-      <main>
-        <SectionFuneralSet />
-      </main>
-
+        <main>
+          <SectionTitleForPage {...titleProps} />
+          <SectionFuneralSet />
+        </main>
+      <Footer />
     </>
   );
 };
