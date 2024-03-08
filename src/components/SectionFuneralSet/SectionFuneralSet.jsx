38f89153f5aa67acdba740b7db5357c8ce4bc1fb
myusername <chyharov@gmail.com>
2024-03-09 00:02:36 +0200

    UPDATE: markup and styles on SectionFuneralSet

--- src/components/SectionFuneralSet/SectionFuneralSet.jsx
+++ src/components/SectionFuneralSet/SectionFuneralSet.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import Modal from '../ModalWindow/ModalWindow'
+import PhotoSlider from "components/PhotoSlider/PhotoSlider";
+import ButtonMoreDetails from 'components/ButtonMoreDetails/ButtonMoreDetails';
 import s from './SectionFuneralSet.module.scss'
 
 const funeralSetPictureList = [
@@ -19,56 +19,42 @@ const funeralSetPictureList = [
         { id: 14, src: require('../../images/funeralSet/funeralSet14.webp'), alt: 'Зображення 14' },
 ];
 
-const SectionFuneralSet = () => {
-  const [modalOpen, setModalOpen] = useState(false);
-  const [selectedImage, setSelectedImage] = useState(null);
+const buttonShowAll = 'Переглянути всі';
 
-  const openModal = (id) => {
-    setSelectedImage(funeralSetPictureList.find(image => image.id === id));
-    setModalOpen(true);
-    document.body.style.overflow = 'hidden';
-  };
+const SectionFuneralSet = () => {
 
     return (
         <section className={s.funeralVases}>
-            <div className={'container ' + s.funeralVasesContainer}>
-                <div className={s.funeralVasesDescriptionContainer}>
-                    <p className="description" style={{marginBottom: '20px' }}>Ритуальне приладдя – це невід’ємний атрибут традиційного обряду поховання православного християнина. Без такого похоронного набору неможливо провести церковне відспівування покійного. Похоронний будинок «Альфа» бере на себе весь клопіт з організації похорону, у тому числі й у такому делікатному питанні, як дотримання заупокійних церковних обрядів.</p>
-                    <h2 className="title" style={{ marginBottom: '20px' }}>Що входить до похоронного набору?</h2>
-                    
-                    <ul className={s.funeralVases__list}>
-                        <li className={s.funeralVases__listItem}><p className="description" style={{marginBottom: '20px' }}>Під час відспівування покійний християнин має бути в нижньому хрестику, в його руках має бути хрест, а на голові налобний віночок зі святими образами. Все це ритуальне приладдя входить до стандартного похоронного набору, який можна купити у православному храмі або замовити у нас.</p></li>
-                        <li className={s.funeralVases__listItem}><p className="description" style={{marginBottom: '20px' }}>Під час церемонії відспівування чи то в церкві, чи в жалобній залі з виїздом священика, всі рідні покійного повинні тримати в руках палаючі церковні свічки, які також входять до звичайного похоронного набору ритуального приладдя.</p></li>
-                        <li className={s.funeralVases__listItem}><p className="description" style={{marginBottom: '20px' }}>Читання священиком дозвільної молитви є ще одним важливим елементом обряду заупокійного відспівування. Текст дозвільної молитви рідні вкладають у руки покійного. Роздрукований текст молитви зі святими образами також входить до стандартного похоронного набору.</p></li>
-                        <li className={s.funeralVases__listItem}><p className="description" style={{marginBottom: '20px' }}>Православні ритуальні приналежності також включають ікону Спасителя або Богоматері з немовлям Ісусом.</p></li>
-                    </ul>
+          <div className={'container ' + s.funeralVasesContainer}>
+                
+          <h2 className="title" style={{ marginBottom: '16px', textAlign: 'center' }}>Поховальний набір</h2>
+          
+          <p className="description" style={{ marginBottom: '64px' }}>Ритуальне приладдя – це невід’ємний атрибут традиційного обряду поховання православного християнина. Без такого поховального набору неможливо провести церковне відспівування покійного. Поховальний будинок «Альфа» бере на себе весь клопіт з організації похорону, у тому числі й у такому делікатному питанні, як дотримання заупокійних церковних обрядів.</p>
 
-                    <h2 className="title" style={{ marginBottom: '20px' }}>Саван та похоронні покривала</h2>
+          <h2 className="title" style={{ marginBottom: '16px', textAlign: 'center' }}>Що входить до поховального набору?</h2>
 
-                    <p className="description" style={{marginBottom: '20px' }}>Православний обряд поховання передбачає вбрання померлого перед похованням у білий одяг. Це символізує перетворення душі покійного і зв’язок його зі Спасителем, який після дива на горі Фавор засяяв білим шатами. Також це символізує духовну чистоту та дотримання всіх обітниць, даних при хрещенні. Адже не дарма саван схожий на хрестильні шати чи рушник для немовлят.</p>
+          <ul className="list" style={{ marginBottom: '64px' }}>
+            <li className="listItem"><p className="description">Під час відспівування покійний християнин має бути в нижньому хрестику, в його руках має бути хрест, а на голові налобний віночок зі святими образами. Все це ритуальне приладдя входить до стандартного поховального набору, який можна купити у православному храмі або замовити у нас.</p></li>
+            <li className="listItem"><p className="description">Під час церемонії відспівування чи то в церкві, чи в жалобній залі з виїздом священика, всі рідні покійного повинні тримати в руках палаючі церковні свічки, які також входять до звичайного поховального набору ритуального приладдя.</p></li>
+            <li className="listItem"><p className="description">Читання священиком дозвільної молитви є ще одним важливим елементом обряду заупокійного відспівування. Текст дозвільної молитви рідні вкладають у руки покійного. Роздрукований текст молитви зі святими образами також входить до стандартного поховального набору.</p></li>
+            <li className="listItem"><p className="description">Православні ритуальні приналежності також включають ікону Спасителя або Богоматері з немовлям Ісусом.</p></li>
+          </ul>
 
-                    <p className="description" style={{marginBottom: '20px' }}>Однак цієї традиції дотримуються не буквально. Символізм білих шат перенесений на похоронний саван – спеціальне біле або світле покривала, яким накривають покійного в труні. На ньому вишивають або друкують ритуальні образи та молитви. Іноді використовують просто світлий тюль чи атласні покривала без релігійної символіки, якими накривають покійного.</p>
-                   
-                    <p className="description" style={{marginBottom: '20px' }}><b>Похоронний будинок «Альфа»</b> співпрацює з прямими виробниками текстильних покривал, саванів та наборів ритуального приладдя, яке виготовляють у Києві та інших містах України. Саме тому ми пропонуємо всі похоронні набори за доступними цінами від виробника, не спекулюючи на горі та релігійних почуттях рідних покійного.</p>
+          <h2 className="title" style={{ marginBottom: '16px', textAlign: 'center' }}>Саван та поховальні покривала</h2>
 
-                    <ul className={s.funeralVasesPictureList}>
-                    {funeralSetPictureList.map((image) => (
-                        <li className={s.funeralVasesPictureList__item} key={image.id}>
-                            <img className={s.funeralVasesPictureList_img} id={image.id} src={image.src} alt={image.alt} onClick={() => openModal(image.id, image.src, image.alt)}/>
-                        </li>
-                        ))}
-                    </ul>
-            
-                        {modalOpen && selectedImage && (
-                        <Modal
-                          selectedImage={selectedImage}
-                          arrayPhoto={funeralSetPictureList}
-                          setSelectedImage={setSelectedImage}
-                          setModalOpen={setModalOpen}
-                        />
-                        )}           
-                </div>
-            </div>
+          <p className="description" style={{ marginBottom: '16px' }}>Православний обряд поховання передбачає вбрання померлого перед похованням у білий одяг. Це символізує перетворення душі покійного і зв’язок його зі Спасителем, який після дива на горі Фавор засяяв білим шатами. Також це символізує духовну чистоту та дотримання всіх обітниць, даних при хрещенні. Адже не дарма саван схожий на хрестильні шати чи рушник для немовлят.</p>
+
+          <p className="description" style={{ marginBottom: '16px' }}>Однак цієї традиції дотримуються не буквально. Символізм білих шат перенесений на поховальний саван – спеціальне біле або світле покривала, яким накривають покійного в труні. На ньому вишивають або друкують ритуальні образи та молитви. Іноді використовують просто світлий тюль чи атласні покривала без релігійної символіки, якими накривають покійного.</p>
+
+          <p className="description" style={{ marginBottom: '56px' }}>Поховальний будинок «Альфа» співпрацює з прямими виробниками текстильних покривал, саванів та наборів ритуального приладдя, яке виготовляють у Києві та інших містах України. Саме тому ми пропонуємо всі поховальні набори за доступними цінами від виробника, не спекулюючи на горі та релігійних почуттях рідних покійного.</p>
+
+          <h2 className="title" style={{ marginBottom: '16px', textAlign: 'center' }}>Поховальний набір</h2>
+
+            <PhotoSlider array={funeralSetPictureList} />   
+
+            <ButtonMoreDetails buttonDescription={buttonShowAll} />
+
+          </div>
         </section>
     );
   };
--- src/pages/FuneralSetPage/FuneralSetPage.jsx
+++ src/pages/FuneralSetPage/FuneralSetPage.jsx
@@ -1,6 +1,6 @@
 import Header from '../../components/Header/Header'
 import SectionTitleForPage from '../../components/SectionTitleForPage/SectionTitleForPage';
-import backgroundImg from '../../images/wreaths/WreathsBackgroundMob.jpg';
+import backgroundImg from '../../images/otherAttributes/OtherAttributesBackgroundMob.jpg';
 import SectionFuneralSet from 'components/SectionFuneralSet/SectionFuneralSet';
 import Footer from 'components/Footer/Footer';
 
