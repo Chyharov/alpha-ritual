93bea9f38e368c4f3c9bf661a96b176fb343b66a
Oleksii Chyharov <chyharov@gmail.com>
2024-05-10 23:53:13 +0200

    UPDATE: SectionHero, markup and styles for desk version

--- src/components/SectionHero/SectionHero.jsx
+++ src/components/SectionHero/SectionHero.jsx
@@ -1,4 +1,7 @@
 import ButtonGetConsultation from 'components/ButtonGetConsultation/ButtonGetConsultation';
+import heroImg from '../../images/pictHero.png'
+import line from '../../images/Line5.svg'
+import blur from '../../images/blur.png'
 import s from '../SectionHero/SectionHero.module.scss';
 
 const SectionHero = () => {
@@ -11,12 +14,18 @@ const SectionHero = () => {
             <h2 className={s.titleForHero}>похоронний дім</h2>
           </div>
           
-          <p style={{ padding: '8px', marginBottom: '40px' }} className='description'>Похоронний дім Альфа має у своєму розпорядженні всі необхідні товари та послуги, що дозволяють проводити поховання або кремацію будь-якого рівня.</p>
+          <p className={'description ' + s.heroDescription}>Похоронний дім Альфа має у своєму розпорядженні всі необхідні товари та послуги, що дозволяють проводити поховання або кремацію будь-якого рівня.</p>
         
-          <ButtonGetConsultation />
+          <div className={s.lineContainer}>
+            <img className={s.lineImage} src={line} alt="line" />
+
+            <ButtonGetConsultation />
+          </div>
           
         </div>
+        <img className={s.hero__image} src={heroImg} alt="hero__image" />
       </div>
+      <img className={s.blur} src={blur} alt="blur" />
     </section>
   );
 };
