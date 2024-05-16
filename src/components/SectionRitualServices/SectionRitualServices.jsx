4a38cfd609662fdb6ee5ff19e8bbfe9d7d1c8acc
Oleksii Chyharov <chyharov@gmail.com>
2024-05-16 21:46:55 +0200

    ADDED: pictures, styles and markup on SectionRitualServices

--- src/components/SectionRitualServices/SectionRitualServices.jsx
+++ src/components/SectionRitualServices/SectionRitualServices.jsx
@@ -6,8 +6,31 @@ const SectionRitualServices = () => {
         <section className={s.SectionRitualServices}>
             <div className='container'>
 
-                <h2 className={s.ritualGoodsListTitle}>Ритуальні послуги</h2>
+                <h2 className='title' style={{ marginBottom: '40px', textAlign: 'center' }}>Ритуальні послуги</h2>
                 
+                <div className={s.ritualServicesContainer}>
+                
+                    <div>
+                        <h2 className={s.ritualListTitle} style={{ textAlign: 'end' }}>Поховання</h2>
+                        <ul className={s.ritualServicesList}>
+                            <li>Економ</li>
+                            <li>Стандарт</li>
+                            <li>VIP</li>
+                        </ul>
+                    </div>
+
+                    <div className={s.cremationContainer}>
+                        <h2 className={s.ritualListTitle}>Кремація</h2>
+                        <ul className={s.ritualServicesList}>
+                            <li>Економ</li>
+                            <li>Стандарт</li>
+                            <li>VIP</li>
+                        </ul>
+                    </div>
+                
+                </div>
+                
+
             </div>
         </section>
     );
