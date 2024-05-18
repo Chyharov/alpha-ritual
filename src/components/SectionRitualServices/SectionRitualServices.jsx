100fffce6a5f852705a26375ee7a0c825c8bd43c
Oleksii Chyharov <chyharov@gmail.com>
2024-05-18 11:16:57 +0200

    UPDATE: markup and styles on SectionRitualServices

--- src/components/SectionRitualServices/SectionRitualServices.jsx
+++ src/components/SectionRitualServices/SectionRitualServices.jsx
@@ -10,27 +10,26 @@ const SectionRitualServices = () => {
                 
                 <div className={s.ritualServicesContainer}>
                 
-                    <div>
+                    <div className={s.ritualContainer}>
                         <h2 className={s.ritualListTitle} style={{ textAlign: 'end' }}>Поховання</h2>
                         <ul className={s.ritualServicesList}>
-                            <li>Економ</li>
-                            <li>Стандарт</li>
-                            <li>VIP</li>
+                            <li style={{ marginRight: '16px' }} className={s.ritualServiceListItem}><div className={s.containerRitualServiceListItem}>Економ</div></li>
+                            <li style={{ marginRight: '16px' }} className={s.ritualServiceListItem}><div className={s.containerRitualServiceListItem}>Стандарт</div></li>
+                            <li className={s.ritualServiceListItem}><div className={s.containerRitualServiceListItem}>VIP</div></li>
                         </ul>
                     </div>
 
                     <div className={s.cremationContainer}>
                         <h2 className={s.ritualListTitle}>Кремація</h2>
-                        <ul className={s.ritualServicesList}>
-                            <li>Економ</li>
-                            <li>Стандарт</li>
-                            <li>VIP</li>
+                        <ul className={s.crimationServicesList}>
+                            <li style={{ marginRight: '16px' }} className={s.ritualServiceListItem}><div className={s.containerRitualServiceListItem}>Економ</div></li>
+                            <li style={{ marginRight: '16px' }} className={s.ritualServiceListItem}><div className={s.containerRitualServiceListItem}>Стандарт</div></li>
+                            <li className={s.ritualServiceListItem}><div className={s.containerRitualServiceListItem}>VIP</div></li>
                         </ul>
                     </div>
                 
                 </div>
                 
-
             </div>
         </section>
     );
