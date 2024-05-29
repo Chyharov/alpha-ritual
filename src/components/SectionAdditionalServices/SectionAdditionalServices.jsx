1f4114e2295a26e7c90b8d1b45fc6580fddffd1d
Oleksii Chyharov <chyharov@gmail.com>
2024-05-29 16:24:17 +0200

    ADDED: styles on SectionAdditationServices

--- src/components/SectionAdditionalServices/SectionAdditionalServices.jsx
+++ src/components/SectionAdditionalServices/SectionAdditionalServices.jsx
@@ -13,6 +13,15 @@ const additionalServices = [
   { id: 7, title: 'Інша атрибутика', link: '/otherattributes' },
 ];
 
+const additionalServicesDesktop = [
+  { id: 1, title: 'Вінки', link: '/wreaths' },
+  { id: 2, title: 'Труни', link: '/coffins' },
+  { id: 3, title: 'Хрести', link: '/crosses' },
+  { id: 4, title: 'Покривала', link: '/covered' },
+  { id: 5, title: 'Урни для праху', link: '/funeralvases' },
+  { id: 6, title: 'Декор', link: '/decorations' },
+];
+
 const buttonDescription = 'Переглянути всі';
 
 const SectionAdditionalServices = () => {
@@ -45,18 +54,22 @@ const SectionAdditionalServices = () => {
       
       <div className={'container ' + s.ritualServicesDesktop__container}>
 
-        <h2 className='title' style={{ marginBottom: '24px' }}>Додаткові послуги</h2>
+        <div className={s.additationContainer}>
+        <h2 className='title' style={{ marginBottom: '40px' }}>Додаткові послуги</h2>
 
         <ul className={s.ritualServices__list}>
-          {additionalServices.map((item) => (
-            <li className={s.ritualServices__listItem} key={item.id}>
+          {additionalServicesDesktop.map((item) => (
+            <li className={s.ritualServices__listItemDesktop} key={item.id}>
               <div className={s.ritualServices__border}>
                 <h2 className={s.ritualServices__title}>{item.title}</h2>
                 <Link className={s.ritualServices__link} to={item.link}>Деталі</Link>
               </div>
             </li>
-          ))}       
-        </ul>     
+          ))}    
+        </ul> 
+
+        </div>
+        
         
       </div>
     </section>
