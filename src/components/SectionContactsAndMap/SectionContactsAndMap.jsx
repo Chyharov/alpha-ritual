0a86d2ec05ab1356054c7aa310845ff5cd195187
Oleksii Chyharov <chyharov@gmail.com>
2024-06-19 16:32:05 +0200

    UPDATE: styles and markup on SectionContactsAndMap for desktop version

--- src/components/SectionContactsAndMap/SectionContactsAndMap.jsx
+++ src/components/SectionContactsAndMap/SectionContactsAndMap.jsx
@@ -4,15 +4,14 @@ import s from './SectionContactsAndMap.module.scss';
 const SectionContactsAndMap = () => {
   return (
     <section className={s.sectionContactsAndMap}>
-      <div className={'container ' + s.contactsAndMap__container}>
-        <div>
-          <h1>Контакти</h1>
-
+        <div className={'container ' + s.contactsAndMap__container}>
+              
           <address>
-            <h2>Адреса</h2>
-            <ul>
+          <h2 className={s.contactTitle}>Контакти</h2>
+            <h3 className={s.addressTitle}>Адреса</h3>
+            <ul className={s.addressList}>
               <li className={s.contactsAddressItem}>
-                <a
+                <a className={s.addressDescription}
                   target="_blank"
                   rel="noopener noreferrer"
                   href="https://maps.app.goo.gl/JeAMk5y2yX5DnuHFA"
@@ -21,7 +20,7 @@ const SectionContactsAndMap = () => {
                 </a>
               </li>
               <li className={s.contactsAddressItem}>
-                <a
+                <a className={s.addressDescription}
                   target="_blank"
                   rel="noopener noreferrer"
                   href="https://maps.app.goo.gl/AwovKjfD7p53WbF56"
@@ -30,32 +29,31 @@ const SectionContactsAndMap = () => {
                 </a>
               </li>
             </ul>
-            <h2>Телефон</h2>
-            <ul>
-              <li>
+            <h3 className={s.addressTitle}>Телефон</h3>
+            <ul className={s.addressList}>
+              <li className={s.telDescription}>
                 <a href="tel:+380672459505">+38 (067) 245 95 05</a>
               </li>
-              <li>
+              <li className={s.telDescription}>
                 <a href="tel:+380442322408">+38 (044) 232 24 08</a>
               </li>
-              <li>
+              <li className={s.telDescription}>
                 <a href="tel:+380995062408">+38 (099) 506 24 08</a>
               </li>
-              <li>
+              <li className={s.telDescription}>
                 <a href="tel:+380667858127">+38 (066) 785 81 27</a>
               </li>
             </ul>
 
-            <p>Щоденно | Цілодобово</p>
-          </address>
-
+            <p style={{ marginBottom: '10px'}} className={s.addressTitle}>Щоденно | Цілодобово</p>
           <ButtonGetConsultation />
-        </div>
-
+        </address>
+              
         <div className={s.contactsMapContainer}>
-          <h2>на мані</h2>
+          <h3 className={s.addressTitle}>на мапі</h3>
           <div className={s.mapContainer}></div>
         </div>
+              
       </div>
     </section>
   );
