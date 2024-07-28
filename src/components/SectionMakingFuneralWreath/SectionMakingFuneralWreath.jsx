a590e89bb2b567e217f98a071905881316e24610
Chyharov <Chyharov@gmail.com>
2024-07-28 07:59:19 +0200

    UPDATE: styles for images on SectionMakingFuneralWreath

--- src/components/SectionMakingFuneralWreath/SectionMakingFuneralWreath.jsx
+++ src/components/SectionMakingFuneralWreath/SectionMakingFuneralWreath.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import fronImgMakingFuneralWreath from '../../images/makingFuneralWreath/fronImgMakingFuneralWreath.jpg';
-import fronImgMakingFuneralWreath2 from '../../images/makingFuneralWreath/fronImgMakingFuneralWreath2.jpg';
+import fronImgMakingFuneralWreathSecond from '../../images/makingFuneralWreath/fronImgMakingFuneralWreathSecond.jpg';
 import rectangleMakingFuneralWreath from '../../images/makingFuneralWreath/rectangleMakingFuneralWreath.png';
 import blurMakingFuneralWreath from '../../images/makingFuneralWreath/blurMakingFuneralWreath.png';
 import s from './SectionMakingFuneralWreath.jsx.module.scss';
@@ -42,11 +42,11 @@ const SectionMakingFuneralWreath = () => {
         <p className='description' style={{ width: '676px', marginBottom: '20px' }}>Для людей творчої натури та померлим у поважному віці використовують квіти червоних та темно-рожевих відтінків. Білі та фіолетові відтінки використовують у вінках на похороні молодих жінок.</p>
         <p className='description' style={{ width: '676px' }}>Флористи поховального будинку «Альфа» точно знають усі правила та традиції складання ритуальних вінків. Ви можете купити як окремо вінок на поховання з доставкою по Києву, так і замовити комплексне флористичне оформлення церемонії прощання.</p>
       
+      </div>
         <img className={s.fronImgMakingFuneralWreath} src={fronImgMakingFuneralWreath} alt="fronImgMakingFuneralWreath" />
-        <img className={s.fronImgMakingFuneralWreath2} src={fronImgMakingFuneralWreath2} alt="fronImgMakingFuneralWreath2" />
+        <img className={s.fronImgMakingFuneralWreathSecond} src={fronImgMakingFuneralWreathSecond} alt="fronImgMakingFuneralWreathSecond" />
         <img className={s.rectangleMakingFuneralWreath} src={rectangleMakingFuneralWreath} alt="rectangleMakingFuneralWreath" />
         <img className={s.blurMakingFuneralWreath} src={blurMakingFuneralWreath} alt="blurMakingFuneralWreath" />
-      </div>
     </section>
   );
 };
