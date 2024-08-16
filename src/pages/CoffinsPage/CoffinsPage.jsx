e603130f9a5caf32489282fe1076fb11a7a90c16
Chyharov <Chyharov@gmail.com>
2024-08-16 23:52:14 +0200

    ADDED: SectionTitleForDefaultPageDesktop to CoffinsPage

--- src/pages/CoffinsPage/CoffinsPage.jsx
+++ src/pages/CoffinsPage/CoffinsPage.jsx
@@ -1,8 +1,20 @@
 import Header from '../../components/Header/Header'
 import SectionTitleForPage from '../../components/SectionTitleForPage/SectionTitleForPage';
-import backgroundImg from '../../images/coffins/backgroundCoffinsMob.jpg'
-import SectionCoffins from '../../components/SectionCoffins/SectionCoffins'
+import SectionTitleForDefaultPageDesktop from 'components/SectionTitleForDefaultPageDesktop/SectionTitleForDefaultPageDesktop';
+import backgroundImgDesktop from 'images/coffins/coffinsBackgroundDesk.png';
+import backgroundImg from '../../images/coffins/backgroundCoffinsMob.jpg';
+import SectionCoffins from '../../components/SectionCoffins/SectionCoffins';
 import Footer from 'components/Footer/Footer';
+
+const titleProps = {
+  titleLink: 'Труни',
+  backgroundImg: backgroundImg,
+  prevTitleLink: 'Додаткові послуги',
+  backgroundImgDesktop: backgroundImgDesktop,
+  prevLink: '/additionalservices',
+  title: 'Труни',
+  description: 'Поховальний дім «Альфа» – це власне виробництво ритуальних трун, які виготовляються із якісної міцної деревини з використанням декоративних оббивних тканин вітчизняного та європейського виробництва. Ми не спекулюємо на горі, тому ціни на всі наші труни, включаючи бюджетні варіанти та елітні двокришкові саркофаги, одні з найдоступніших у Києві.'
+};
 
 const title = 'Труни';
 
@@ -12,6 +24,7 @@ const CoffinsPage = () => {
       <Header />
         <main>
           <SectionTitleForPage title={title} backgroundImg={backgroundImg} />
+          <SectionTitleForDefaultPageDesktop {...titleProps}/>
           <SectionCoffins />
         </main>
       <Footer />
