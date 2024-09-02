d9e4fabc3832d88b235171acf69ca4542aa1b4d8
Chyharov <Chyharov@gmail.com>
2024-09-02 23:47:11 +0200

    ADDED: SectionTitleForDefaultPageDesktop to DecorationsPage

--- src/pages/DecorationsPage/DecorationsPage.jsx
+++ src/pages/DecorationsPage/DecorationsPage.jsx
@@ -1,18 +1,29 @@
 import Header from '../../components/Header/Header'
 import SectionTitleForPage from '../../components/SectionTitleForPage/SectionTitleForPage';
+import SectionTitleForDefaultPageDesktop from 'components/SectionTitleForDefaultPageDesktop/SectionTitleForDefaultPageDesktop';
 import SectionDecorations from 'components/SectionDecorations/SectionDecorations';
+import backgroundImgDesktop from 'images/crosses/crossesBackgroundDesk.png';
+import backgroundImg from '../../images/crosses/CrossesBackgroundMob.jpg';
 import Footer from 'components/Footer/Footer';
 
 const titleProps = {
+  titleLink: 'Декор',
+  backgroundImg: backgroundImg,
+  prevTitleLink: 'Додаткові послуги',
+  backgroundImgDesktop: backgroundImgDesktop,
+  prevLink: '/additionalservices',
   title: 'Декор',
+  description: 'До встановлення постійного пам’ятника одразу після поховання на могилі хрещеного християнина ставлять ритуальний хрест, на якому закріплено табличку з прізвищем, ім’ям та датами народження та смерті померлого. Це давня християнська традиція, особливо шанована у православному світі. Поховальний дім «Альфа» бере на себе клопіт із виготовлення та встановлення ритуального дерев’яного чи металевого хреста, який повністю відповідає церковним канонам та культурним традиціям нашого народу.'
 };
+
 
 const DecorationsPage = () => {
   return (
     <>
       <Header />
         <main>
-          <SectionTitleForPage {...titleProps} />
+        <SectionTitleForPage {...titleProps} />
+        <SectionTitleForDefaultPageDesktop {...titleProps} />
           <SectionDecorations />
         </main>
       <Footer />
