14faf45ca1450ffb2fba4c553cdec177626cc685
Chyharov <Chyharov@gmail.com>
2024-09-03 23:19:39 +0200

    ADDED: SectionTitleForDefaultPageDesktop to FuneralSetPage

--- src/pages/FuneralSetPage/FuneralSetPage.jsx
+++ src/pages/FuneralSetPage/FuneralSetPage.jsx
@@ -1,12 +1,19 @@
 import Header from '../../components/Header/Header'
 import SectionTitleForPage from '../../components/SectionTitleForPage/SectionTitleForPage';
+import SectionTitleForDefaultPageDesktop from 'components/SectionTitleForDefaultPageDesktop/SectionTitleForDefaultPageDesktop';
 import backgroundImg from '../../images/otherAttributes/OtherAttributesBackgroundMob.jpg';
+import backgroundImgDesktop from 'images/crosses/crossesBackgroundDesk.png';
 import SectionFuneralSet from 'components/SectionFuneralSet/SectionFuneralSet';
 import Footer from 'components/Footer/Footer';
 
 const titleProps = {
+  titleLink: 'Поховальний набір',
+  backgroundImg: backgroundImg,
+  prevTitleLink: 'Додаткові послуги',
+  backgroundImgDesktop: backgroundImgDesktop,
+  prevLink: '/additionalservices',
   title: 'Поховальний набір',
-  backgroundImg: backgroundImg
+  description: 'Ритуальне приладдя – це невід’ємний атрибут традиційного обряду поховання православного християнина. Без такого поховального набору неможливо провести церковне відспівування покійного. Поховальний будинок «Альфа» бере на себе весь клопіт з організації похорону, у тому числі й у такому делікатному питанні, як дотримання заупокійних церковних обрядів.'
 };
 
 const FuneralSetPage = () => {
@@ -14,7 +21,8 @@ const FuneralSetPage = () => {
     <>
       <Header />
         <main>
-          <SectionTitleForPage {...titleProps} />
+        <SectionTitleForPage {...titleProps} />
+        <SectionTitleForDefaultPageDesktop {...titleProps} />
           <SectionFuneralSet />
         </main>
       <Footer />
