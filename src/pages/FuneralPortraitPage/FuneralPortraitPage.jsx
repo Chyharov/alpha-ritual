238c0a9d1cdd1efa988b260905a59c14a05d601f
Chyharov <Chyharov@gmail.com>
2024-09-07 23:17:14 +0200

    ADDED:SectionTitleForDefaultPageDesktop for FuneralPortraitPage

--- src/pages/FuneralPortraitPage/FuneralPortraitPage.jsx
+++ src/pages/FuneralPortraitPage/FuneralPortraitPage.jsx
@@ -1,12 +1,19 @@
 import Header from '../../components/Header/Header'
 import SectionTitleForPage from '../../components/SectionTitleForPage/SectionTitleForPage';
+import SectionTitleForDefaultPageDesktop from 'components/SectionTitleForDefaultPageDesktop/SectionTitleForDefaultPageDesktop';
 import backgroundImg from '../../images/otherAttributes/FuneralPortraitBackgroundMob.jpg';
+import backgroundImgDesktop from 'images/otherAttributes/funeralPortraitBackgroundDesk.png';
 import SectionFuneralPortrait from 'components/SectionFuneralPortrait/SectionFuneralPortrait';
 import Footer from 'components/Footer/Footer';
 
 const titleProps = {
+  titleLink: 'Портрет на поховання',
+  backgroundImg: backgroundImg,
+  prevTitleLink: 'Додаткові послуги',
+  backgroundImgDesktop: backgroundImgDesktop,
+  prevLink: '/additionalservices',
   title: 'Портрет на поховання',
-  backgroundImg: backgroundImg
+  description: 'У канонах православної віри, як і в інших світових релігіях, немає жодної згадки про необхідність наявності на поховальній церемонії прижиттєвого портрета покійного. Немає і обов’язкового перев’язування портрета чорною траурною стрічкою. Це виключно світська традиція, яка має глибоке значення.'
 };
 
 const FuneralPortraitPage = () => {
@@ -14,7 +21,8 @@ const FuneralPortraitPage = () => {
     <>
       <Header />
         <main>
-          <SectionTitleForPage {...titleProps} />
+        <SectionTitleForPage {...titleProps} />
+        <SectionTitleForDefaultPageDesktop {...titleProps} />
           <SectionFuneralPortrait />
         </main>
       <Footer />
