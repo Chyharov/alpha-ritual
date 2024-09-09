30502da82b35527f80fba9c7453dc86ac57624da
Chyharov <Chyharov@gmail.com>
2024-09-09 22:08:49 +0200

    ADDED: SectionTitleForDefaultPageDesktop to FuneralVasesPage

--- src/pages/FuneralVasesPage/FuneralVasesPage.jsx
+++ src/pages/FuneralVasesPage/FuneralVasesPage.jsx
@@ -1,12 +1,19 @@
 import Header from '../../components/Header/Header'
 import SectionTitleForPage from '../../components/SectionTitleForPage/SectionTitleForPage';
+import SectionTitleForDefaultPageDesktop from 'components/SectionTitleForDefaultPageDesktop/SectionTitleForDefaultPageDesktop';
 import backgroundImg from '../../images/funeralVases/VasesBackgroundMob.jpg';
+import backgroundImgDesktop from 'images/funeralVases/VasesBackgroundDesk.png';
 import SectionFuneralVases from 'components/SectionFuneralVases/SectionFuneralVases';
 import Footer from 'components/Footer/Footer';
 
 const titleProps = {
+  titleLink: 'Урни для праху',
+  backgroundImg: backgroundImg,
+  prevTitleLink: 'Додаткові послуги',
+  backgroundImgDesktop: backgroundImgDesktop,
+  prevLink: '/additionalservices',
   title: 'Урни для праху',
-  backgroundImg: backgroundImg
+  description: 'Кремація стає все більш популярною ритуальною послугою в Києві через порівняно меншу вартість, ніж при традиційному похованні, та віддаленість міських цвинтарів. Процес кремування померлого відбувається у крематорії, де створено всі умови для проведення церемонії прощання.'
 };
 
 const FuneralVasesPage = () => {
@@ -14,7 +21,8 @@ const FuneralVasesPage = () => {
     <>
       <Header />
         <main>
-          <SectionTitleForPage {...titleProps} />
+        <SectionTitleForPage {...titleProps} />
+        <SectionTitleForDefaultPageDesktop {...titleProps} />
           <SectionFuneralVases />
         </main>
       <Footer />
