a67177a2cc748b5d09b5b1bde5382b6779776c32
Chyharov <Chyharov@gmail.com>
2024-09-09 22:13:41 +0200

    ADDED: SectionGalleryForDesktop to FuneralVasesPage

--- src/components/SectionFuneralVases/SectionFuneralVases.jsx
+++ src/components/SectionFuneralVases/SectionFuneralVases.jsx
@@ -3,21 +3,9 @@ import PhotoSlider from "components/PhotoSlider/PhotoSlider";
 import ButtonMoreDetails from 'components/ButtonMoreDetails/ButtonMoreDetails';
 import s from './SectionFuneralVases.module.scss'
 
-const funeralVasesPictureList = [
-    { id: 1, src: require('../../images/funeralVases/funeralVases1.jpg'), alt: 'Зображення 1' },
-    { id: 2, src: require('../../images/funeralVases/funeralVases2.jpg'), alt: 'Зображення 2' },
-    { id: 3, src: require('../../images/funeralVases/funeralVases3.jpg'), alt: 'Зображення 3' },
-    { id: 4, src: require('../../images/funeralVases/funeralVases4.jpg'), alt: 'Зображення 4' },
-    { id: 5, src: require('../../images/funeralVases/funeralVases5.jpg'), alt: 'Зображення 5' },
-    { id: 6, src: require('../../images/funeralVases/funeralVases6.jpg'), alt: 'Зображення 6' },
-    { id: 7, src: require('../../images/funeralVases/funeralVases7.jpg'), alt: 'Зображення 7' },
-    { id: 8, src: require('../../images/funeralVases/funeralVases8.jpg'), alt: 'Зображення 8' },
-    { id: 9, src: require('../../images/funeralVases/funeralVases9.jpg'), alt: 'Зображення 9' },
-  ];
-
 const buttonShowAll = 'Переглянути всі';
 
-const SectionFuneralVases = () => {
+const SectionFuneralVases = ({ array }) => {
 
   return (
       <section className={s.funeralVases}>
@@ -27,7 +15,7 @@ const SectionFuneralVases = () => {
             
           <h2 className="title" style={{ marginBottom: '16px', textAlign: 'center' }}>Урни для праху</h2>
         
-          <PhotoSlider array={funeralVasesPictureList} />         
+          <PhotoSlider array={array} />         
 
           <Link to="/funeralvasesgallery" style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
             <ButtonMoreDetails buttonDescription={buttonShowAll} />
--- src/pages/FuneralVasesPage/FuneralVasesPage.jsx
+++ src/pages/FuneralVasesPage/FuneralVasesPage.jsx
@@ -4,7 +4,23 @@ import SectionTitleForDefaultPageDesktop from 'components/SectionTitleForDefault
 import backgroundImg from '../../images/funeralVases/VasesBackgroundMob.jpg';
 import backgroundImgDesktop from 'images/funeralVases/VasesBackgroundDesk.png';
 import SectionFuneralVases from 'components/SectionFuneralVases/SectionFuneralVases';
+import SectionGalleryForDesktop from 'components/SectionGalleryForDesktop/SectionGalleryForDesktop';
 import Footer from 'components/Footer/Footer';
+
+const funeralVasesPictureList = [
+    { id: 1, src: require('../../images/funeralVases/funeralVases1.jpg'), alt: 'Зображення 1' },
+    { id: 2, src: require('../../images/funeralVases/funeralVases2.jpg'), alt: 'Зображення 2' },
+    { id: 3, src: require('../../images/funeralVases/funeralVases3.jpg'), alt: 'Зображення 3' },
+    { id: 4, src: require('../../images/funeralVases/funeralVases4.jpg'), alt: 'Зображення 4' },
+    { id: 5, src: require('../../images/funeralVases/funeralVases5.jpg'), alt: 'Зображення 5' },
+    { id: 6, src: require('../../images/funeralVases/funeralVases6.jpg'), alt: 'Зображення 6' },
+    { id: 7, src: require('../../images/funeralVases/funeralVases7.jpg'), alt: 'Зображення 7' },
+    { id: 8, src: require('../../images/funeralVases/funeralVases8.jpg'), alt: 'Зображення 8' },
+    { id: 9, src: require('../../images/funeralVases/funeralVases9.jpg'), alt: 'Зображення 9' },
+  ];
+
+const buttonDescription = 'Переглянути всі';
+const titleForGallery = 'Вінки із живих квітів';
 
 const titleProps = {
   titleLink: 'Урни для праху',
@@ -21,9 +37,14 @@ const FuneralVasesPage = () => {
     <>
       <Header />
         <main>
-        <SectionTitleForPage {...titleProps} />
-        <SectionTitleForDefaultPageDesktop {...titleProps} />
-          <SectionFuneralVases />
+          <SectionTitleForPage {...titleProps} />
+          <SectionTitleForDefaultPageDesktop {...titleProps} />
+          <SectionFuneralVases array={funeralVasesPictureList} />
+          <SectionGalleryForDesktop
+            array={funeralVasesPictureList}
+            buttonDescription={buttonDescription}
+            titleForGallery={titleForGallery}
+          />
         </main>
       <Footer />
     </>
