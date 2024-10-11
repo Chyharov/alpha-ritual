6fa214e894c645157e6cf81b6263086426224c54
Chyharov <Chyharov@gmail.com>
2024-10-11 12:12:58 +0200

    UPDATE: added phone numbers on SectionHero for desktop version

--- src/components/SectionHero/SectionHero.jsx
+++ src/components/SectionHero/SectionHero.jsx
@@ -13,6 +13,15 @@ const SectionHero = () => {
             <h1 className={s.titleHero}>АЛЬФА-РИТУАЛ</h1>
             <h2 className={s.titleForHero}>похоронний дім</h2>
           </div>
+
+          <nav className={s.hero__navigation}>
+            <ul className={s.heroNavlist}>
+              <li className={s.heroNavlist__item}><a href="tel:+380672459505">+38 (067) 245 95 05</a></li>
+              <li className={s.heroNavlist__item}><a href="tel:+380442322408">+38 (044) 232 24 08</a></li>
+              <li className={s.heroNavlist__item}><a href="tel:+380995069473">+38 (099) 506 94 73</a></li>
+              <li className={s.heroNavlist__item}><a href="tel:+380667858127">+38 (066) 785 81 27</a></li>
+            </ul>
+          </nav>
           
           <p className={'description ' + s.heroDescription}>Похоронний дім Альфа має у своєму розпорядженні всі необхідні товари та послуги, що дозволяють проводити поховання або кремацію будь-якого рівня.</p>
           
