abcde8e74b45b749c56e2a4e8c621f3af476ef7c
Chyharov <chyharov@gmail.com>
2024-11-26 23:32:04 +0100

    ADDED: new phone number for ModalWindow and Footer

--- src/components/ButtonGetConsultation/ButtonGetConsultation.jsx
+++ src/components/ButtonGetConsultation/ButtonGetConsultation.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { FaTelegram } from 'react-icons/fa6';
 import { FaViber } from 'react-icons/fa';
 import { FaSquareWhatsapp } from 'react-icons/fa6';
-import closeIcon from '../../images/closeIcon2.svg'
+import closeIcon from '../../images/closeIcon2.svg';
 import s from './ButtonGetConsultation.module.scss';
 
 const ButtonGetConsultation = ({ style }) => {
@@ -39,89 +39,93 @@ const ButtonGetConsultation = ({ style }) => {
   });
   return (
     <>
-      <button style={style} type="button" className={s.getConsultation} onClick={openModal}>
+      <button
+        style={style}
+        type="button"
+        className={s.getConsultation}
+        onClick={openModal}
+      >
         Отримати консультацію
       </button>
 
-          {isModalOpen && (
+      {isModalOpen && (
         <div className={s.bgShadow} onClick={handleOutsideClick}>
           <div className={s.modal}>
-            <img className={s.btn__close} src={closeIcon} alt="closeIcon" onClick={closeModal}/>
-            
+            <img
+              className={s.btn__close}
+              src={closeIcon}
+              alt="closeIcon"
+              onClick={closeModal}
+            />
+
             <div className={s.modalTitleContainer}>
-                <h2 className={s.titleNeedHelp}>Потрібна допомога?</h2>
-                    <div className={s.border}></div>
-                <h3 className={s.titleCallUs}>Зателефонуйте нам</h3>
+              <h2 className={s.titleNeedHelp}>Потрібна допомога?</h2>
+              <div className={s.border}></div>
+              <h3 className={s.titleCallUs}>Зателефонуйте нам</h3>
             </div>
 
+            <ul className={s.phoneNumberList}>
+              <a className={s.phoneNumberListLink} href="tel:+380672459505">
+                <li className={s.phoneNumberItemMain}>+38 (067) 245 95 05</li>
+              </a>
+              <a className={s.phoneNumberListLink} href="tel:+380442322408">
+                <li className={s.phoneNumberItem}>+38 (044) 232 24 08</li>
+              </a>
+              <a className={s.phoneNumberListLink} href="tel:+380995069473">
+                <li className={s.phoneNumberItem}>+38 (099) 506 94 73</li>
+              </a>
+              <a className={s.phoneNumberListLink} href="tel:+380667858127">
+                <li className={s.phoneNumberItem}>+38 (066) 785 81 27</li>
+              </a>
+              <a className={s.phoneNumberListLink} href="tel:+3800939545423">
+                <li className={s.phoneNumberItem}>+38 (093) 954 54 23</li>
+              </a>
+            </ul>
 
-                <ul className={s.phoneNumberList}>
-                    <a className={s.phoneNumberListLink} href="tel:+380672459505">
-                        <li className={s.phoneNumberItemMain}>
-                            +38 (067) 245 95 05
-                        </li>
-                    </a>
-                    <a className={s.phoneNumberListLink} href="tel:+380442322408">
-                        <li className={s.phoneNumberItem}>
-                            +38 (044) 232 24 08
-                        </li>
-                    </a>
-                    <a className={s.phoneNumberListLink} href="tel:+380995069473">
-                        <li className={s.phoneNumberItem}>
-                            +38 (099) 506 94 73
-                        </li>
-                    </a>
-                    <a className={s.phoneNumberListLink} href="tel:+380667858127">
-                        <li className={s.phoneNumberItem}>
-                            +38 (066) 785 81 27
-                        </li>
-                    </a>
-                </ul>
+            <ul className={s.contact__list}>
+              <li className={s.contact__item}>
+                <a
+                  className={s.contact__links}
+                  href="https://t.me/+380672459505"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaTelegram
+                    className={`${s.contact__icons} ${s.icon__telegram}`}
+                  />
+                  <p className={s.contact__itemDescription}> Telegram</p>
+                </a>
+              </li>
 
-                <ul className={s.contact__list}>
-                  <li className={s.contact__item}>
-                    <a
-                      className={s.contact__links}
-                      href="https://t.me/+380672459505"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FaTelegram
-                        className={`${s.contact__icons} ${s.icon__telegram}`}
-                      />
-                      <p className={s.contact__itemDescription}> Telegram</p>
-                    </a>
-                  </li>
+              <li className={s.contact__item}>
+                <a
+                  className={s.contact__links}
+                  href="viber://chat?number=+380672459505/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaViber className={`${s.contact__icons} ${s.icon__viber}`} />
+                  <p className={s.contact__itemDescription}> Viber</p>
+                </a>
+              </li>
 
-                  <li className={s.contact__item}>
-                    <a
-                      className={s.contact__links}
-                      href="viber://chat?number=+380672459505/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FaViber className={`${s.contact__icons} ${s.icon__viber}`} />
-                      <p className={s.contact__itemDescription}> Viber</p>
-                    </a>
-                  </li>
-
-                  <li className={s.contact__item}>
-                    <a
-                      className={s.contact__links}
-                      href="whatsapp://send?phone=+380672459505"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FaSquareWhatsapp
-                        className={`${s.contact__icons} ${s.icon__whatsApp}`}
-                      />
-                      <p className={s.contact__itemDescription}> WhatsApp</p>
-                    </a>
-                  </li>
-              </ul>
+              <li className={s.contact__item}>
+                <a
+                  className={s.contact__links}
+                  href="whatsapp://send?phone=+380672459505"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaSquareWhatsapp
+                    className={`${s.contact__icons} ${s.icon__whatsApp}`}
+                  />
+                  <p className={s.contact__itemDescription}> WhatsApp</p>
+                </a>
+              </li>
+            </ul>
             <p className={s.contact__itemDescription}>Щоденно | Цілодобово</p>
+          </div>
         </div>
-      </div>
       )}
     </>
   );
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import ScrollToTopButton from 'components/ScrollToTopButton/ScrollToTopButton';
 import Logo from '../Logo/Logo';
 import ButtonGetConsultation from 'components/ButtonGetConsultation/ButtonGetConsultation';
@@ -8,72 +8,118 @@ const Footer = () => {
   return (
     <footer className={s.footer}>
       <div className={'container ' + s.footerContainer}>
-        
         <div className={s.navigationContainer}>
           <Logo />
 
           <nav className={s.footerNavigation}>
             <ul className={s.footerNavList}>
-              <Link to="/"><li className={s.footerNavListItem}>Головна</li></Link>
-              <Link to="/about"><li className={s.footerNavListItem}>Про компанію</li></Link>
-              <Link to="/ritualgoods"><li className={s.footerNavListItem}>Ритуальні товари</li></Link>
-              <Link to="/ritualtransport"><li className={s.footerNavListItem}>Ритуальний транспорт</li></Link>
-              <Link to="/ritualservices"><li className={s.footerNavListItem}>Ритуальні послуги</li></Link>
-              <Link to="/additionalservices"><li className={s.footerNavListItem}>Додаткові послуги</li></Link>
+              <Link to="/">
+                <li className={s.footerNavListItem}>Головна</li>
+              </Link>
+              <Link to="/about">
+                <li className={s.footerNavListItem}>Про компанію</li>
+              </Link>
+              <Link to="/ritualgoods">
+                <li className={s.footerNavListItem}>Ритуальні товари</li>
+              </Link>
+              <Link to="/ritualtransport">
+                <li className={s.footerNavListItem}>Ритуальний транспорт</li>
+              </Link>
+              <Link to="/ritualservices">
+                <li className={s.footerNavListItem}>Ритуальні послуги</li>
+              </Link>
+              <Link to="/additionalservices">
+                <li className={s.footerNavListItem}>Додаткові послуги</li>
+              </Link>
             </ul>
           </nav>
         </div>
-        
 
-          <address className={s.address}>
-            <h2 className={s.addressTitle}>Адреса</h2>
-            <ul className={s.addressList}>
-              <a href="https://maps.app.goo.gl/JeAMk5y2yX5DnuHFA" target="_blank" rel="noopener noreferrer">
-                <li style={{ padding: '8px 8px 4px' }} className={s.addressListItem}>
-                  Київ, вул. Івана Федорова, 33
-                </li>
-              </a>
-              <a href="https://maps.app.goo.gl/AwovKjfD7p53WbF56" target="_blank" rel="noopener noreferrer">
-                <li style={{ padding: '4px 8px 8px', marginBottom: '8px' }} className={s.addressListItem}>
-                  м.Київ, вул. Колоскова 9 (район Совського кладовища)
-                </li>
-              </a>
-              <a href="tel:+380672459505">
-                <li style={{ padding: '4px 8px' }} className={s.addressPhoneListItemLast}>
-                  +38 (067) 245 95 05
-                </li>
-              </a>
-              <a href="tel:+380442322408">
-                <li style={{ padding: '4px 8px' }} className={s.addressPhoneListItem}>
-                  +38 (044) 232 24 08
-                </li>
-              </a>
-              <a href="tel:+380995069473">
-                <li style={{ padding: '4px 8px' }} className={s.addressPhoneListItem}>
-                  +38 (099) 506 94 73
-                </li>
-              </a>
-              <a href="tel:+380667858127">
-                <li style={{ padding: '4px 8px', marginBottom: '8px' }} className={s.addressPhoneListItem}>
-                  +38 (066) 785 81 27
-                </li>
-              </a>
-            </ul>
+        <address className={s.address}>
+          <h2 className={s.addressTitle}>Адреса</h2>
+          <ul className={s.addressList}>
+            <a
+              href="https://maps.app.goo.gl/JeAMk5y2yX5DnuHFA"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <li
+                style={{ padding: '8px 8px 4px' }}
+                className={s.addressListItem}
+              >
+                Київ, вул. Івана Федорова, 33
+              </li>
+            </a>
+            <a
+              href="https://maps.app.goo.gl/AwovKjfD7p53WbF56"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <li
+                style={{ padding: '4px 8px 8px', marginBottom: '8px' }}
+                className={s.addressListItem}
+              >
+                м.Київ, вул. Колоскова 9 (район Совського кладовища)
+              </li>
+            </a>
+            <a href="tel:+380672459505">
+              <li
+                style={{ padding: '4px 8px' }}
+                className={s.addressPhoneListItemLast}
+              >
+                +38 (067) 245 95 05
+              </li>
+            </a>
+            <a href="tel:+380442322408">
+              <li
+                style={{ padding: '4px 8px' }}
+                className={s.addressPhoneListItem}
+              >
+                +38 (044) 232 24 08
+              </li>
+            </a>
+            <a href="tel:+380995069473">
+              <li
+                style={{ padding: '4px 8px' }}
+                className={s.addressPhoneListItem}
+              >
+                +38 (099) 506 94 73
+              </li>
+            </a>
+            <a href="tel:+380995069473">
+              <li
+                style={{ padding: '4px 8px' }}
+                className={s.addressPhoneListItem}
+              >
+                +38 (066) 785 81 27
+              </li>
+            </a>
+            <a href="tel:+380939545423">
+              <li
+                style={{ padding: '4px 8px', marginBottom: '8px' }}
+                className={s.addressPhoneListItem}
+              >
+                +38 (093) 954 54 23
+              </li>
+            </a>
+          </ul>
           <p className={s.footerAllTimeDescription}>Щоденно | Цілодобово</p>
-          </address>
-        
+        </address>
+
         <div className={s.containerButtonGetConsultation}>
           <div className={s.btnContainer}>
-           <ButtonGetConsultation />
+            <ButtonGetConsultation />
           </div>
         </div>
- 
-              
-          <div className={s.copyrightDescriptionContainer}>
-            <p className={s.copyrightDescription}>АЛЬФА - Похоронний дім © 2023</p> <p className={s.copyrightDescription}>Всі права захищені.</p>
-          </div>
+
+        <div className={s.copyrightDescriptionContainer}>
+          <p className={s.copyrightDescription}>
+            АЛЬФА - Похоронний дім © 2023
+          </p>{' '}
+          <p className={s.copyrightDescription}>Всі права захищені.</p>
+        </div>
       </div>
-        
+
       <ScrollToTopButton />
     </footer>
   );
