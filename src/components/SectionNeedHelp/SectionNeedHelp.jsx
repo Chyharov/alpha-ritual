f3e3ad4812316ec41fcae4287c7486dc6d34f57f
Chyharov <chyharov@gmail.com>
2024-12-02 22:43:48 +0100

    ADDED: social links and update styles

--- src/components/SectionNeedHelp/SectionNeedHelp.jsx
+++ src/components/SectionNeedHelp/SectionNeedHelp.jsx
@@ -1,3 +1,6 @@
+import { FaTelegram } from 'react-icons/fa6';
+import { FaViber } from 'react-icons/fa';
+import { FaSquareWhatsapp } from 'react-icons/fa6';
 import s from './SectionNeedHelp.module.scss';
 
 const SectionNeedHelp = () => {
@@ -14,7 +17,7 @@ const SectionNeedHelp = () => {
 
             <ul className={s.phoneNumberList}>
               <a className={s.phoneNumberListLink} href="tel:+380672459505">
-                <li className={s.phoneNumberItem}>+38 (067) 245 95 05</li>
+                <li className={s.phoneNumberItem} style={{color: '#866929'}}>+38 (067) 245 95 05</li>
               </a>
               <a className={s.phoneNumberListLink} href="tel:+380442322408">
                 <li className={s.phoneNumberItem}>+38 (044) 232 24 08</li>
@@ -29,6 +32,49 @@ const SectionNeedHelp = () => {
                 <li className={s.phoneNumberItem}>+38 (093) 954 54 23</li>
               </a>
             </ul>
+
+            <ul className={s.contact__list}>
+              <li className={s.contact__item}>
+                <a
+                  className={s.contact__links}
+                  href="https://t.me/+380672459505"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaTelegram
+                    className={`${s.contact__icons} ${s.icon__telegram}`}
+                  />
+                  <p className={s.contact__itemDescription}> Telegram</p>
+                </a>
+              </li>
+
+              <li className={s.contact__item}>
+                <a
+                  className={s.contact__links}
+                  href="viber://chat?number=+380672459505/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaViber className={`${s.contact__icons} ${s.icon__viber}`} />
+                  <p className={s.contact__itemDescription}> Viber</p>
+                </a>
+              </li>
+
+              <li className={s.contact__item}>
+                <a
+                  className={s.contact__links}
+                  href="whatsapp://send?phone=+380672459505"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaSquareWhatsapp
+                    className={`${s.contact__icons} ${s.icon__whatsApp}`}
+                  />
+                  <p className={s.contact__itemDescription}> WhatsApp</p>
+                </a>
+              </li>
+            </ul>
+            <p className={s.contact__itemDescription}>Щоденно | Цілодобово</p>
           </div>
         </div>
       </div>
