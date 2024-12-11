a176384ca6e7960e4d8de2a47d2d974e14fbd56c
Chyharov <chyharov@gmail.com>
2024-12-11 23:30:07 +0100

    UPDATE: SectionContactsAndMap

--- src/components/SectionContactsAndMap/SectionContactsAndMap.jsx
+++ src/components/SectionContactsAndMap/SectionContactsAndMap.jsx
@@ -1,42 +1,41 @@
 import ButtonGetConsultation from 'components/ButtonGetConsultation/ButtonGetConsultation';
-import phoneIcon from 'images/phoneIcon.svg'
-import locationIcon from 'images/locationIcon.svg'
-import blurAddressAndMap from 'images/blurAddressAndMap.svg'
-import MapComponent from 'components/MapComponent/MapComponent'
+import phoneIcon from '../../images/phoneIcon.svg';
+import locationIcon from '../../images/locationIcon.svg';
+import blurAddressAndMap from 'images/blurAddressAndMap.svg';
+import MapComponent from 'components/MapComponent/MapComponent';
 import s from './SectionContactsAndMap.module.scss';
 
 const SectionContactsAndMap = () => {
   return (
-    <section className={s.sectionContactsAndMap} id='сontacts'>
-        <div className={'container ' + s.contactsAndMap__container}>
-              
-          <address>
+    <section className={s.sectionContactsAndMap} id="сontacts">
+      <div className={'container ' + s.contactsAndMap__container}>
+        <address>
           <h2 className={s.contactTitle}>Контакти</h2>
           <h3 className={s.addressTitle}>Адреса</h3>
           <div className={s.phoneNumbersContainer}>
             <img className={s.contactsIcon} src={locationIcon} alt="locationIcon" />
-              <ul className={s.addressList}>
-                <li className={s.addressDescription}>
-                  <a
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href="https://maps.app.goo.gl/JeAMk5y2yX5DnuHFA"
-                  >
-                    м.Київ, вул. Івана Федорова, 33
-                  </a>
-                </li>
-                <li className={s.addressDescription}>
-                  <a
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href="https://maps.app.goo.gl/AwovKjfD7p53WbF56"
-                  >
-                    м.Київ, вул. Колоскова 9 (район Совського кладовища)
-                  </a>
-                </li>
+            <ul className={s.addressList}>
+              <li className={s.addressDescription}>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://maps.app.goo.gl/JeAMk5y2yX5DnuHFA"
+                >
+                  м.Київ, вул. Івана Федорова, 33
+                </a>
+              </li>
+              <li className={s.addressDescription}>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://maps.app.goo.gl/AwovKjfD7p53WbF56"
+                >
+                  м.Київ, вул. Колоскова 9 (район Совського кладовища)
+                </a>
+              </li>
             </ul>
           </div>
-            
+
           <h3 className={s.addressTitle}>Телефон</h3>
           <div className={s.phoneNumbersContainer}>
             <img className={s.contactsIcon} src={phoneIcon} alt="phoneIcon" />
@@ -58,21 +57,24 @@ const SectionContactsAndMap = () => {
               </li>
             </ul>
           </div>
-          
 
-            <p style={{ marginBottom: '10px'}} className={s.addressTitle}>Щоденно | Цілодобово</p>
+          <p style={{ marginBottom: '10px' }} className={s.addressTitle}>
+            Щоденно | Цілодобово
+          </p>
           <ButtonGetConsultation />
         </address>
 
         <div className={s.contactsMapContainer}>
           <h3 className={s.addressTitle}>на мапі</h3>
-          <MapComponent />
+          {typeof window !== 'undefined' && <MapComponent />}
         </div>
-
       </div>
 
-      <img className={s.blurAddressAndMap} src={blurAddressAndMap} alt="blurAddressAndMap" />
-
+      <img
+        className={s.blurAddressAndMap}
+        src={blurAddressAndMap}
+        alt="blurAddressAndMap"
+      />
     </section>
   );
 };
