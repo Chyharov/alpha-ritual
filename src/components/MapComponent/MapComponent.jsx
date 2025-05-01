c599700ce292e0cfc3a26a8b7b89e09a9b0a889c
Chyharov <chyharov@gmail.com>
2025-05-01 22:31:06 +0200

    FIX: logic for MapComponent

--- src/components/MapComponent/MapComponent.jsx
+++ src/components/MapComponent/MapComponent.jsx
@@ -5,16 +5,24 @@ const MapComponent = () => {
   const mapRef = useRef(null);
 
   useEffect(() => {
-    const map = new window.google.maps.Map(mapRef.current, {
-      center: { lat: 50.427392, lng: 30.5086165 },
-      zoom: 17,
-    });
+  const interval = setInterval(() => {
+    if (window.google && window.google.maps && mapRef.current) {
+      const map = new window.google.maps.Map(mapRef.current, {
+        center: { lat: 50.427392, lng: 30.5086165 },
+        zoom: 17,
+      });
 
-    new window.google.maps.Marker({
-      position: { lat: 50.427392, lng: 30.5086165 },
-      map
-    });
-  }, []);
+      new window.google.maps.Marker({
+        position: { lat: 50.427392, lng: 30.5086165 },
+        map
+      });
+
+      clearInterval(interval); // Зупиняємо інтервал після ініціалізації
+    }
+  }, 100); // Перевіряємо кожні 100мс
+
+  return () => clearInterval(interval);
+}, []);
 
   return <div className={s.mapContainer} ref={mapRef} />;
 };
