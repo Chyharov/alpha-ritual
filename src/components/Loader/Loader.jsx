6d91c7d1146b394a16ea09e41e7e07adcf2cffef
chyharov <chyharov@gmail.com>
2025-06-07 08:40:30 +0200

    update: styles for Loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { RotatingLines } from 'react-loader-spinner';
+import s from './loader.module.scss';
 
 const Loader = () => {
   return (
-    <div style={{
+    <div className={s.loader__container} style={{
       display: 'flex',
       justifyContent: 'center',
       alignItems: 'center',
       height: '100vh',
     }}>
       <RotatingLines
-        strokeColor="black"
+        strokeColor="#5E4715"
         strokeWidth="5"
         animationDuration="0.75"
         width="96"
